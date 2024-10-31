1d413850672fcf62ccd9e02d1d055600a9af5e45
mamepon2580 <g.poso.2580@gmail.com>
2024-11-01 08:44:18 +0900

    feat: Add selection

--- repositorys/discordjs/registration.js
+++ repositorys/discordjs/registration.js
@@ -37,11 +37,79 @@ const commands = [
       option.setName('source')
         .setDescription('string')
         .setRequired(true)
+        .addChoices(
+          { name: 'Arabic', value: 'AR'},
+          // { name: 'Bulgarian', value: 'BG'},
+          { name: 'Czech', value: 'CS'},
+          { name: 'Danish', value: 'DA'},
+          { name: 'German', value: 'DE'},
+          { name: 'Greek', value: 'EL'},
+          { name: 'English', value: 'EN'},
+          { name: 'Spanish', value: 'ES'},
+          { name: 'Estonian', value: 'ET'},
+          { name: 'Finnish', value: 'ET'},
+          { name: 'French', value: 'FR'},
+          // { name: 'Hungarian', value: 'HU'},
+          { name: 'Indonesian', value: 'ID'},
+          { name: 'Italian', value: 'IT'},
+          { name: 'Japanese', value: 'JA'},
+          { name: 'Korean', value: 'KO'},
+          // { name: 'Lithuanian', value: 'LT'},
+          { name: 'Latvian', value: 'LV'},
+          { name: 'Norwegian', value: 'NB'},
+          { name: 'Dutch', value: 'NL'},
+          { name: 'Polish', value: 'PL'},
+          // { name: 'Portuguese', value: 'PT'},
+          // { name: 'Romanian', value: 'RO'},
+          // { name: 'Russian', value: 'RU'},
+          // { name: 'Slovak', value: 'SK'},
+          // { name: 'Slovenian', value: 'SL'},
+          { name: 'Swedish', value: 'SV'},
+          { name: 'Turkish', value: 'TR'},
+          { name: 'Ukrainian', value: 'UK'},
+          { name: 'Chinese', value: 'ZH'}
+        )
     )
     .addStringOption(option =>
       option.setName('target')
         .setDescription('string')
         .setRequired(true)
+        .addChoices(
+          { name: 'Arabic', value: 'AR'},
+          // { name: 'Bulgarian', value: 'BG'},
+          { name: 'Czech', value: 'CS'},
+          { name: 'Danish', value: 'DA'},
+          { name: 'German', value: 'DE'},
+          { name: 'Greek', value: 'EL-GB'},
+          { name: 'English (British)', value: 'EN-GB'},
+          { name: 'English (American)', value: 'EN-US'},
+          { name: 'Spanish', value: 'ES'},
+          { name: 'Estonian', value: 'ET'},
+          { name: 'Finnish', value: 'ET'},
+          { name: 'French', value: 'FI'},
+          { name: 'French', value: 'FR'},
+          // { name: 'Hungarian', value: 'HU'},
+          { name: 'Indonesian', value: 'ID'},
+          { name: 'Italian', value: 'IT'},
+          { name: 'Japanese', value: 'JA'},
+          { name: 'Korean', value: 'KO'},
+          // { name: 'Lithuanian', value: 'LT'},
+          { name: 'Latvian', value: 'LV'},
+          { name: 'Norwegian', value: 'NB'},
+          { name: 'Dutch', value: 'NL'},
+          { name: 'Polish', value: 'PL'},
+          // { name: 'Portuguese (Brazilian)', value: 'PT-BR'},
+          // { name: 'Portuguese (all Portuguese variants excluding Brazilian Portuguese)', value: 'PT-PT'},
+          // { name: 'Romanian', value: 'RO'},
+          // { name: 'Russian', value: 'RU'},
+          // { name: 'Slovak', value: 'SK'},
+          // { name: 'Slovenian', value: 'SL'},
+          { name: 'Swedish', value: 'SV'},
+          { name: 'Turkish', value: 'TR'},
+          { name: 'Ukrainian', value: 'UK'},
+          { name: 'Chinese (simplified)', value: 'ZH'},
+          { name: 'Chinese (traditional)', value: 'ZH'}
+        )
     )
     .addStringOption(option =>
       option.setName('message')
