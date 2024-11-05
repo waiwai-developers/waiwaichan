09f517a291c55d3fd7657e04b5bba916a3c70ced
mamepon2580 <g.poso.2580@gmail.com>
2024-11-05 14:34:33 +0900

    feat: Modifying settings around the database

--- models/index.js
+++ models/index.js
@@ -8,13 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, { host: config.host, port: config.port, dialect: config.dialect });
 
 fs
   .readdirSync(__dirname)
