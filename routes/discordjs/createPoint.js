bf6c9b199b6a2c3285edb75f10bf791b2c65e16e
mamepon2580 <g.poso.2580@gmail.com>
2024-11-07 01:27:13 +0900

    feat: Avoiding double registration

--- routes/discordjs/createPoint.js
+++ routes/discordjs/createPoint.js
@@ -12,16 +12,19 @@ client.on("", () => {
 client.on('messageReactionAdd', async (reaction, user) => {
   if (user.bot) return;
   if (reaction.message.author.bot) return;
-  // if (user.id === reaction.message.author.bot) return;
+  if (user.id === reaction.message.author.id) return;
 
   if (reaction.emoji.name === '🍬') {
     const date = new Date()
     const points = await models.Point.findAndCountAll({
+      attributes: ['messageId'],
       where: {
         giveUserId: user.id,
         createdAt: {[Sequelize.Op.gte]: date.setDate(date.getDate() -1)}
       }
     });
+
+    if (points.rows.map((p) => p.messageId).includes(reaction.message.id)) return;
 
     if (points.count > 2) {
       await reaction.message.channel.send(`もう今日はスタンプを押してもポイントは増えないよ！っ`);
