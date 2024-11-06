828ed6d210f107a4327420b6f5731fe2aeac1f0f
mamepon2580 <g.poso.2580@gmail.com>
2024-11-06 12:54:01 +0900

    feat: Add date validation

--- logics/reminderSet.mjs
+++ logics/reminderSet.mjs
@@ -1,13 +1,20 @@
 import models from '../models/index.js'
 import dayjs from 'dayjs';
+import moment from 'moment';
 
 export const reminderSet = async (channelId, userId, message, datetime) => {
 	try {
+
+		const remindAt = dayjs(datetime).subtract(9, 'h').format('YYYY-MM-DD HH:mm:ss');
+		const datenow = moment().format('YYYY-MM-DD HH:mm:ss');
+
+		if (remindAt <= datenow) return "過去の日付のリマインドは設定できないよ！っ"
+
 		await models.Reminder.create({
 			channelId: channelId,
 			userId: userId,
 			message: message,
-			remindAt: dayjs(datetime).subtract(9, 'h').format('YYYY-MM-DD HH:mm:ss')
+			remindAt: remindAt
 		});
 		return "リマインドの投稿を予約したよ！っ";
 	} catch (e) {
