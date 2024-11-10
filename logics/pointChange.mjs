1bae7433753be5e3a39500d3ecd50e815f5cf0d6
wautopia <gameprogrammer55@gmail.com>
2024-11-11 02:43:32 +0900

    feat : fixed update query

--- logics/pointChange.mjs
+++ logics/pointChange.mjs
@@ -11,7 +11,10 @@ export const pointChange = async (userId, userItemId) => {
 		//userIdが引数のuserId
 		//itemIdが引数のitemId
 		const date = new Date();
-		const userItem = await UserItem.findOne({
+		//userItemのstatusを0から1に更新する
+		const userItem = await UserItem.update(
+			{ status: UserItem.STATUS_INVALID },
+			{
 			where: {
 				id: userItemId,
 				userId: userId,
@@ -22,10 +25,8 @@ export const pointChange = async (userId, userItemId) => {
 		});
 
 		//userItemが取れなかった場合に”アイテムは持ってないよ！っ"を返す
-		if (!userItem) return "アイテムは持ってないよ！っ";
-		//userItemのstatusを0から1に更新する
-		await userItem.update({ status: UserItem.STATUS_INVALID });
-		return "アイテムと交換したよ！っ";
+		if (userItem[0] > 0) return "アイテムと交換したよ！っ";
+		return "アイテム持ってないよ！っ";
 	} catch (e) {
 		console.error("Error:", e);
 		return "エラーが起こったよ！っ";
