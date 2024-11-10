f27e8ac1f4383ffbf37de2a35d630e7f2b914528
mamepon2580 <g.poso.2580@gmail.com>
2024-11-10 18:42:58 +0900

    feat: Categorize your help

--- logics/help.mjs
+++ logics/help.mjs
@@ -3,16 +3,21 @@ import config from "../config/commands.json" with { type: "json" };
 export const help = () => {
 	try {
 		const texts = config.map((c) =>
-			[
-				`- \`${c.name}\``,
-				`  - 値　　： ${c.parameter}`,
-				`  - 例　　： ${c.example}`,
-				`  - 説明　： ${c.description}`,
-			].join("\n"),
-		);
+			[`## ${c.category.name}`,
+				c.category.commands.map((command) =>
+					[
+						`- \`${command.name}\``,
+						`  - 値　　： ${command.parameter}`,
+						`  - 例　　： ${command.example}`,
+						`  - 説明　： ${command.description}`
+					].join("\n")
+				).join("\n")
+			].join("\n")
+		)
 		return texts.join("\n");
 	} catch (e) {
 		console.error("Error:", e);
 		return ("エラーが起こったよ！っ");
 	}
 };
+
