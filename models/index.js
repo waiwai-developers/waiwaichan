fd23ea4ca2c3b0ac4df9bc7eba44999cfa8aa0cb
y1wtt <awataki1206@gmail.com>
2024-11-10 18:27:32 +0900

    refactor: replacing models to ESM

--- models/index.js
+++ models/index.js
@@ -1,36 +1,15 @@
-'use strict';
+import { Item } from "./item";
+import { Point } from "./point";
+import { Reminder } from "./reminder";
+import { UserItem } from "./useritem.js";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-const sequelize = new Sequelize(config.database, config.username, config.password, { host: config.host, port: config.port, dialect: config.dialect });
+const models = { UserItem, Item, Point, Reminder };
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+for (const item of Object.keys(models)) {
+	if (models[item].associate) {
+		models[item].associate(models);
+	}
+}
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-
-module.exports = db;
+import { MysqlConnector } from "./mysqlConnector.js";
+export { MysqlConnector, UserItem, Item, Point, Reminder };
--- models/point.js
+++ models/point.js
@@ -1,31 +1,34 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Point extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Point.init({
-    receiveUserId: DataTypes.BIGINT,
-    giveUserId: DataTypes.BIGINT,
-    messageId: DataTypes.BIGINT,
-    status: DataTypes.BOOLEAN,
-    expiredAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Point',
-  });
+import { DataTypes, Model } from "sequelize";
+import { MysqlConnector } from "./mysqlConnector.js";
+const sequelize = new MysqlConnector.getInstance();
 
-  Point.STATUS_VALID = 0
-  Point.STATUS_INVALID = 1
+export const Point = (() => {
+	class Point extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Point.init(
+		{
+			receiveUserId: DataTypes.BIGINT,
+			giveUserId: DataTypes.BIGINT,
+			messageId: DataTypes.BIGINT,
+			status: DataTypes.BOOLEAN,
+			expiredAt: DataTypes.DATE,
+		},
+		{
+			sequelize,
+			modelName: "Point",
+		},
+	);
 
-  return Point;
-};
+	Point.STATUS_VALID = 0;
+	Point.STATUS_INVALID = 1;
+
+	return Point;
+})();
