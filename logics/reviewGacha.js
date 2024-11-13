939627c75851afbf0fdac0669e1291cb99b98a43
mamepon2580 <g.poso.2580@gmail.com>
2024-11-14 00:16:21 +0900

    feat: Add probability

--- logics/reviewGacha.js
+++ logics/reviewGacha.js
@@ -12,30 +12,35 @@ export const reviewGacha = async (discordId, pullReqId) => {
 			.map((a) => a.user.githubId);
 
 		const response = await getPullRequest(pullReqId);
-		const pullRequest = response.data
+		const pullRequest = response.data;
 
 		if (
-			pullRequest.user.id !==
-			accounts.find((a) => a.user.githubId === pullRequest.user.id)
+			pullRequest.user.login !==
+			accounts.find((a) => a.user.discordId === discordId).user.githubId
 		) {
 			return "pull reqのオーナーじゃないよ！っ";
 		}
-
-		await reviewPullRequest(reviewerGithubIds, pullReqId);
 
 		if (pullRequest.state !== "open") {
 			return "pull reqのステータスがopenじゃないよ！っ";
 		}
 
+		// NOTE:todo より良い乱数生成に変える
+		const randomNum = Math.floor(Math.random() * reviewerGithubIds.length);
+
+		// NOTE:todo そのうち複数人に対応できるロジックに変える
+		const selectReviewerGithubIds = [reviewerGithubIds[randomNum]];
+		const selectReviewerDiscordIds = [reviewerDiscordIds[randomNum]];
+
+		await reviewPullRequest(selectReviewerGithubIds, pullReqId);
+
 		const pullRequestTitle = pullRequest.title;
-		const pullRequestUrl = pullRequest.url;
-		const issueUrl = pullRequest.issue_url;
+		const pullRequestUrl = pullRequest.html_url;
 
 		const texts = [];
-		texts.push(reviewerDiscordIds.map((r) => `<@${r}>`).join(" "));
-		texts.push(
-			`\n\n${pullRequestTitle}\n- issue: ${pullRequestUrl}\n- pullreq: ${issueUrl}`,
-		);
+		texts.push(selectReviewerDiscordIds.map((r) => `<@${r}>`).join(" "));
+		texts.push("review依頼が来たよ！っ");
+		texts.push(`\n${pullRequestTitle}\npullreq: ${pullRequestUrl}`);
 
 		return texts.join("\n");
 	} catch (e) {
