661c514f84d721805ee42feb43774aad9ee76b5f
y1wtt <awataki1206@gmail.com>
2024-11-14 18:01:47 +0900

    feat: add types to database config.json

---
+++ entities/config/DatabaseConfig.ts
@@ -0,0 +1,17 @@
+import json from "../../config/config.json" with { type: "json" };
+
+interface DatabaseConfigType {
+	username: string;
+	password: string;
+	database: string;
+	host: string;
+	port: number;
+	dialect: string;
+}
+interface DatabaseJsonType {
+	development: DatabaseConfigType;
+	test: DatabaseConfigType;
+	production: DatabaseConfigType;
+}
+
+export const DatabaseConfig: DatabaseJsonType = json;
