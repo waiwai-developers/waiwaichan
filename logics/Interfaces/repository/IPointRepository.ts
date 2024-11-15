a362fa2760afe6d4783ab5c67341b9c65277655b
y1wtt <awataki1206@gmail.com>
2024-11-15 10:44:38 +0900

    feat: define IPointRepository

---
+++ logics/Interfaces/repository/IPointRepository.ts
@@ -0,0 +1,9 @@
+import type { PointDto } from "../../../entities/dto/PointDto";
+import type { DiscordUserId } from "../../../entities/vo/DiscordUserId";
+import type { PointCount } from "../../../entities/vo/PointCount";
+
+export interface IPointRepository {
+	createPoint(data: PointDto): Promise<PointDto>;
+	pointCount(userId: DiscordUserId): Promise<PointCount>;
+	ConsumePoints(userId: DiscordUserId, points: PointCount): Promise<boolean>;
+}
