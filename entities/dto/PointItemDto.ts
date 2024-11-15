d13c81409fa5e203b12397b7c6b179a5ac4f1fdb
y1wtt <awataki1206@gmail.com>
2024-11-15 11:32:45 +0900

    fix: fixed PointItemModel

--- entities/dto/PointItemDto.ts
+++ entities/dto/PointItemDto.ts
@@ -1,17 +1,12 @@
-import { PointItemDescription } from "../vo/PointItemDescription";
-import { PointItemId } from "../vo/PointItemId";
-import { PointItemName } from "../vo/PointItemName";
+import type { DiscordUserId } from "@/entities/vo/DiscordUserId";
+import type { PointItemDescription } from "../vo/PointItemDescription";
+import type { PointItemId } from "../vo/PointItemId";
+import type { PointItemName } from "../vo/PointItemName";
 export class PointItemDto {
 	constructor(
 		public id: PointItemId,
+		public userId: DiscordUserId,
 		public name: PointItemName,
 		public description: PointItemDescription,
 	) {}
-	static from(id: number, name: string, description: string): PointItemDto {
-		return new PointItemDto(
-			new PointItemId(id),
-			new PointItemName(name),
-			new PointItemDescription(description),
-		);
-	}
 }
--- repositories/sequelize-mysql/PointItemRepositoryImpl.ts
+++ repositories/sequelize-mysql/PointItemRepositoryImpl.ts
@@ -1,20 +1,38 @@
+import { PointItemDto } from "@/entities/dto/PointItemDto";
+import { DiscordUserId } from "@/entities/vo/DiscordUserId";
+import { PointItemDescription } from "@/entities/vo/PointItemDescription";
+import { PointItemId } from "@/entities/vo/PointItemId";
+import { PointItemName } from "@/entities/vo/PointItemName";
+import type { IPointItemRepository } from "@/logics/Interfaces/repository/IPointItemRepository";
 import { DataTypes, Model } from "sequelize";
-import { PointItemDto } from "../../entities/dto/PointItemDto";
-import type { PointItemId } from "../../entities/vo/PointItemId";
-import type { IPointItemRepository } from "../../logics/Interfaces/repository/IPointItemRepository";
 import { MysqlConnector } from "./mysqlConnector";
 
 const sequelize = MysqlConnector.getInstance();
 
 class PointItemRepositoryImpl extends Model implements IPointItemRepository {
 	declare id: number;
+	declare userId: string;
 	declare name: string;
 	declare description: string;
 	async findById(id: PointItemId): Promise<PointItemDto | undefined> {
 		return await PointItemRepositoryImpl.findOne({
 			where: { id: id.getValue() },
 		}).then((r) =>
-			r ? PointItemDto.from(r.id, r.name, r.description) : undefined,
+			r ? this.toDto(r.id, r.userId, r.name, r.description) : undefined,
+		);
+	}
+
+	toDto(
+		id: number,
+		discordUserId: string,
+		name: string,
+		description: string,
+	): PointItemDto {
+		return new PointItemDto(
+			new PointItemId(id),
+			new DiscordUserId(discordUserId),
+			new PointItemName(name),
+			new PointItemDescription(description),
 		);
 	}
 }
