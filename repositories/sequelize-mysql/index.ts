a0a3606983d2b79185aad1830e237f366a0a768b
y1wtt <awataki1206@gmail.com>
2024-11-15 11:10:34 +0900

    feat: impl associations

---
+++ repositories/sequelize-mysql/index.ts
@@ -0,0 +1,5 @@
+import { PointItemRepositoryImpl } from "./PointItemRepositoryImpl";
+import { UserPointItemRepositoryImpl } from "./UserPointItemRepositoryImpl";
+import "./associations";
+
+export { PointItemRepositoryImpl, UserPointItemRepositoryImpl };
