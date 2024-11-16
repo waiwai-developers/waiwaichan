b9384807cc2f2444686e99266661cc544bf2761f
y1wtt <awataki1206@gmail.com>
2024-11-17 06:52:23 +0900

    feat: define TranslateTargetLanguage

--- entities/vo/TranslateLanguage.ts
+++
@@ -1,3 +0,0 @@
-import { ValueObject } from "./ValueObject";
-
-export class TranslateLanguage extends ValueObject<string> {}
---
+++ entities/vo/TranslateSourceLanguage.ts
@@ -0,0 +1,3 @@
+import { ValueObject } from "./ValueObject";
+import type {sourceLanguage} from "@/entities/constants/translate";
+export class TranslateSourceLanguage extends ValueObject<sourceLanguage> {}
