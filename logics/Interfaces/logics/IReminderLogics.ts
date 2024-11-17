9ee57f1cdaf86153a2ec8aa735ace07f3e663492
y1wtt <awataki1206@gmail.com>
2024-11-17 11:28:07 +0900

    feat: define IReminderLogics

---
+++ logics/Interfaces/logics/IReminderLogics.ts
@@ -0,0 +1,16 @@
+import type { DiscordChannelId } from "@/entities/vo/DiscordChannelId";
+import type { DiscordMessageId } from "@/entities/vo/DiscordMessageId";
+import type { DiscordUserId } from "@/entities/vo/DiscordUserId";
+import type { RemindTime } from "@/entities/vo/RemindTime";
+import type { ReminderId } from "@/entities/vo/ReminderId";
+
+interface IReminderLogics {
+	create(
+		channel: DiscordChannelId,
+		userId: DiscordUserId,
+		message: DiscordMessageId,
+		remindAt: RemindTime,
+	): string;
+	list(userId: DiscordUserId): string;
+	delete(id: ReminderId): string;
+}
