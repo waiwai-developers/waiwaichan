ddce02ecf639d682406efcff5e0243a8d859d936
y1wtt <awataki1206@gmail.com>
2024-11-17 10:20:08 +0900

    feat: update IUtilityLogics

--- logics/Interfaces/logics/IUtilityLogics.ts
+++ logics/Interfaces/logics/IUtilityLogics.ts
@@ -3,6 +3,7 @@ import type { HelpCategory } from "@/entities/vo/HelpCategory";
 interface IUtilityLogics {
 	waiwai(): string;
 	help(type: HelpCategory): string;
+	choise(): string;
 	dice(): string;
 	parrot(): string;
 }
