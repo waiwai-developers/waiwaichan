755ed836c4de2cd823e9bb23ecd4a28c09005983
y1wtt <awataki1206@gmail.com>
2024-11-22 16:03:45 +0900

    fix: delete only inserted records when down

--- migrator/seeds/20241111041901-item.ts
+++ migrator/seeds/20241111041901-item.ts
@@ -1,20 +1,28 @@
 import type { Seed } from "@/migrator/umzug";
+import { Op } from "sequelize";
 import { MigratePointItemModel } from "./models/MigratePointItemModel";
+const records = [
+	{
+		id: 1,
+		name: "waiwaiオリジナルTシャツ",
+		description: "UTmeで作った2000円くらいのTシャツ",
+	},
+	{
+		id: 2,
+		name: "お菓子",
+		description: "コンビニで買える100円くらいお菓子",
+	},
+];
 export const up: Seed = async () => {
-	await new MigratePointItemModel().bulkFindOrCreate([
-		{
-			id: 1,
-			name: "waiwaiオリジナルTシャツ",
-			description: "UTmeで作った2000円くらいのTシャツ",
-		},
-		{
-			id: 2,
-			name: "お菓子",
-			description: "コンビニで買える100円くらいお菓子",
-		},
-	]);
+	await new MigratePointItemModel().bulkFindOrCreate(records);
 };
 
 export const down: Seed = async ({ context: sequelize }) => {
-	await sequelize.getQueryInterface().bulkDelete("Items", {}, {});
+	await sequelize.getQueryInterface().bulkDelete(
+		"Items",
+		{
+			id: { [Op.in]: records.map((r) => r.id) },
+		},
+		{},
+	);
 };
