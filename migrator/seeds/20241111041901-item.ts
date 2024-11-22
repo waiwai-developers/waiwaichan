d6ffb911292d93e2944838c8963db862aeeea198
y1wtt <awataki1206@gmail.com>
2024-11-22 15:50:29 +0900

    feat: seed file create with upsert

--- migrator/seeds/20241111041901-item.ts
+++ migrator/seeds/20241111041901-item.ts
@@ -1,26 +1,18 @@
 import type { Seed } from "@/migrator/umzug";
-
+import { MigratePointItemModel } from "./models/MigratePointItemModel";
 export const up: Seed = async ({ context: sequelize }) => {
-	await sequelize.getQueryInterface().bulkInsert(
-		"Items",
-		[
-			{
-				id: 1,
-				name: "waiwaiオリジナルTシャツ",
-				description: "UTmeで作った2000円くらいのTシャツ",
-				createdAt: "2024-01-01 00:00:00",
-				updatedAt: "2024-01-01 00:00:00",
-			},
-			{
-				id: 2,
-				name: "お菓子",
-				description: "コンビニで買える100円くらいお菓子",
-				createdAt: "2024-01-01 00:00:00",
-				updatedAt: "2024-01-01 00:00:00",
-			},
-		],
-		{},
-	);
+	await new MigratePointItemModel().bulkFindOrCreate([
+		{
+			id: 1,
+			name: "waiwaiオリジナルTシャツ",
+			description: "UTmeで作った2000円くらいのTシャツ",
+		},
+		{
+			id: 2,
+			name: "お菓子",
+			description: "コンビニで買える100円くらいお菓子",
+		},
+	]);
 };
 
 export const down: Seed = async ({ context: sequelize }) => {
