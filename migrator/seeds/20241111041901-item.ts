a2df98877001dada100ed4189628b400aeb45465
y1wtt <awataki1206@gmail.com>
2024-11-22 15:19:42 +0900

    feat: remove manual ignore inserting

--- migrator/seeds/20241111041901-item.ts
+++ migrator/seeds/20241111041901-item.ts
@@ -1,41 +1,28 @@
 import type { Seed } from "@/migrator/umzug";
-import type { Sequelize } from "sequelize";
 
-const getRecord = async (s: Sequelize) => {
-	const existingData1 =
-		(await s.query("SELECT id FROM Items WHERE id = 1"))[0].length > 0;
-	const existingData2 =
-		(await s.query("SELECT id FROM Items WHERE id = 2"))[0].length > 0;
-	return [
-		existingData1
-			? null
-			: {
-					id: 1,
-					name: "waiwaiオリジナルTシャツ",
-					description: "UTmeで作った2000円くらいのTシャツ",
-					createdAt: "2024-01-01 00:00:00",
-					updatedAt: "2024-01-01 00:00:00",
-				},
-		existingData2
-			? null
-			: {
-					id: 2,
-					name: "お菓子",
-					description: "コンビニで買える100円くらいお菓子",
-					createdAt: "2024-01-01 00:00:00",
-					updatedAt: "2024-01-01 00:00:00",
-				},
-	].filter((it) => it != null);
+export const up: Seed = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().bulkInsert(
+		"Items",
+		[
+			{
+				id: 1,
+				name: "waiwaiオリジナルTシャツ",
+				description: "UTmeで作った2000円くらいのTシャツ",
+				createdAt: "2024-01-01 00:00:00",
+				updatedAt: "2024-01-01 00:00:00",
+			},
+			{
+				id: 2,
+				name: "お菓子",
+				description: "コンビニで買える100円くらいお菓子",
+				createdAt: "2024-01-01 00:00:00",
+				updatedAt: "2024-01-01 00:00:00",
+			},
+		],
+		{},
+	);
 };
 
-const up: Seed = async ({ context: sequelize }) => {
-	const records = await getRecord(sequelize);
-	if (records.length <= 0) {
-		return;
-	}
-	await sequelize.getQueryInterface().bulkInsert("Items", records, {});
-};
-
-const down: Seed = async ({ context: sequelize }) => {
+export const down: Seed = async ({ context: sequelize }) => {
 	await sequelize.getQueryInterface().bulkDelete("Items", {}, {});
 };
