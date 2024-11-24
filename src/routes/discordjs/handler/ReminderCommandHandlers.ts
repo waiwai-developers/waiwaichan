e5fc907864a299e5f6cc13aa65733be5fd03ff9a
y1wtt <awataki1206@gmail.com>
2024-11-24 19:19:34 +0900

    feat: ReminderCommandHandlers

---
+++ src/routes/discordjs/handler/ReminderCommandHandlers.ts
@@ -0,0 +1,81 @@
+import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import { ReminderDto } from "@/src/entities/dto/ReminderDto";
+import { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
+import { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
+import { RemindTime } from "@/src/entities/vo/RemindTime";
+import { ReminderId } from "@/src/entities/vo/ReminderId";
+import { ReminderMessage } from "@/src/entities/vo/ReminderMessage";
+import type { IReminderLogic } from "@/src/logics/Interfaces/logics/IReminderLogic";
+import dayjs from "dayjs";
+import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { inject, injectable } from "inversify";
+import type { SlashCommandHandler } from "./SlashCommandHandler";
+
+@injectable()
+class ReminderSetCommandHandlers implements SlashCommandHandler {
+	@inject(LogicTypes.ReminderLogic)
+	private reminderLogic!: IReminderLogic;
+
+	isHandle(commandName: string): boolean {
+		return commandName === "reminderset";
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+	): Promise<void> {
+		await interaction.reply(
+			await this.reminderLogic.create(
+				new ReminderDto(
+					new ReminderId(0),
+					new DiscordChannelId(interaction.channelId),
+					new DiscordUserId(interaction.user.id),
+					new ReminderMessage(interaction.options.getString("message") ?? ""),
+					new RemindTime(
+						dayjs(interaction.options.getString("datetime"))
+							.subtract(9, "h")
+							.toDate(),
+					),
+				),
+			),
+		);
+	}
+}
+
+@injectable()
+class ReminderDeleteCommandHandlers implements SlashCommandHandler {
+	@inject(LogicTypes.ReminderLogic)
+	private reminderLogic!: IReminderLogic;
+
+	isHandle(commandName: string): boolean {
+		return commandName === "reminderdelete";
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+	): Promise<void> {
+		await interaction.reply(
+			await this.reminderLogic.delete(
+				new ReminderId(interaction.options?.getInteger("id") ?? 0),
+				new DiscordUserId(interaction.user.id),
+			),
+		);
+	}
+}
+
+@injectable()
+class ReminderListCommandHandlers implements SlashCommandHandler {
+	@inject(LogicTypes.ReminderLogic)
+	private reminderLogic!: IReminderLogic;
+
+	isHandle(commandName: string): boolean {
+		return commandName === "reminderlist";
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+	): Promise<void> {
+		await interaction.reply(
+			await this.reminderLogic.list(new DiscordUserId(interaction.user.id)),
+		);
+	}
+}
