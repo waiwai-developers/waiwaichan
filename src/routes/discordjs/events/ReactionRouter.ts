b9def2c4790045b40476505c05f541a248269046
y1wtt <awataki1206@gmail.com>
2024-11-24 18:39:59 +0900

    feat: impl CandyReactionHandler

--- src/routes/discordjs/events/ReactionRouter.ts
+++ src/routes/discordjs/events/ReactionRouter.ts
@@ -1,60 +1,19 @@
-import { AppConfig } from "@/src/entities/config/AppConfig";
-import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
-import { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
-import { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
-import type { IPointLogic } from "@/src/logics/Interfaces/logics/IPointLogic";
 import type { DiscordEventRouter } from "@/src/routes/discordjs/events/DiscordEventRouter";
 import type { Client } from "discord.js";
-import { inject, injectable } from "inversify";
+import { injectable, multiInject } from "inversify";
+import { HandlerTypes } from "../../../entities/constants/DIContainerTypes";
+import type {
+	DiscordEventHandler,
+	ReactionInteraction,
+} from "../handler/DiscordEventHandler";
 
 @injectable()
 export class ReactionRouter implements DiscordEventRouter {
-	@inject(LogicTypes.PointLogic)
-	private pointLogic!: IPointLogic;
+	@multiInject(HandlerTypes.ReactionHandler)
+	private readonly handlers!: DiscordEventHandler<ReactionInteraction>[];
 	register(client: Client): void {
-		client.on("messageReactionAdd", async (reaction, user) => {
-			if (reaction.partial) {
-				try {
-					await reaction.fetch();
-					await reaction.message.fetch();
-				} catch (err) {
-					console.log("fail to fetch old message");
-					return;
-				}
-			}
-			if (user.bot) {
-				console.log("reaction by bot");
-				return;
-			}
-
-			if (
-				(reaction.message.author?.bot ?? true) ||
-				(reaction.message.author?.id ?? null) == null
-			) {
-				console.log("some data is null");
-				return;
-			}
-
-			if (reaction.message.author?.id == null) {
-				console.log("author id is null");
-				return;
-			}
-
-			if (reaction.emoji.name !== AppConfig.backend.pointEmoji) {
-				console.log("not peer bonus emoji");
-				return;
-			}
-
-			const res = await this.pointLogic.givePoint(
-				new DiscordUserId(reaction.message.author.id),
-				new DiscordUserId(user.id),
-				new DiscordMessageId(reaction.message.id),
-			);
-
-			if (!res) {
-				return;
-			}
-			await reaction.message.reply(res);
+		client.on("messageReactionAdd", (reaction, user, details) => {
+			this.handlers.map((h) => h.handle({ reaction, user, details }));
 		});
 	}
 }
--- src/routes/discordjs/handler/DiscordEventHandler.ts
+++ src/routes/discordjs/handler/DiscordEventHandler.ts
@@ -1,3 +1,16 @@
+import type {
+	MessageReaction,
+	MessageReactionEventDetails,
+	PartialMessageReaction,
+	PartialUser,
+	User,
+} from "discord.js";
+
+export type ReactionInteraction = {
+	reaction: MessageReaction | PartialMessageReaction;
+	user: User | PartialUser;
+	details: MessageReactionEventDetails;
+};
 export interface DiscordEventHandler<T> {
 	handle(arg: T): Promise<void>;
 }
