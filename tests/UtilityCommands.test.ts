5dc64ae5075d4e3eca5e1ee4e45233aa6b16c09f
y1wtt <awataki1206@gmail.com>
2024-11-26 00:34:07 +0900

    feat: impl /dice test with decimal number

--- tests/UtilityCommands.test.ts
+++ tests/UtilityCommands.test.ts
@@ -109,4 +109,19 @@ describe("Test UtilityCommand", () => {
 		verify(commandMock.reply("")).never();
 		verify(commandMock.reply(InternalErrorMessage)).once();
 	});
+
+	test("Test /dice parameter:3.14159265", async () => {
+		const commandMock = mockSlashCommand("dice", {
+			parameter: Math.PI,
+		});
+		const TEST_CLIENT = await TestDiscordServer.getClient();
+		let value = 0;
+		when(commandMock.reply(anything())).thenCall((args) => {
+			value = args;
+		});
+		TEST_CLIENT.emit("interactionCreate", instance(commandMock));
+		await waitUntilReply(commandMock);
+		verify(commandMock.reply(anything())).once();
+		verify(commandMock.reply("パラメーターが整数じゃないよ！っ")).once();
+	});
 });
