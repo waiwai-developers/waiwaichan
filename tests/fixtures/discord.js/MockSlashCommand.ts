e16587086d2bcbc8fd20b8a0793519e778898934
y1wtt <awataki1206@gmail.com>
2024-11-25 22:21:52 +0900

    feat: implements options mock into MockSlashCommand.ts

--- tests/fixtures/discord.js/MockSlashCommand.ts
+++ tests/fixtures/discord.js/MockSlashCommand.ts
@@ -1,11 +1,56 @@
 import { DiscordCommandRegister } from "@/src/routes/discordjs/DiscordCommandRegister";
-import { ChatInputCommandInteraction } from "discord.js";
+import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import {
+	ApplicationCommandOption,
+	type CacheType,
+	ChatInputCommandInteraction,
+	type CommandInteractionOptionResolver,
+} from "discord.js";
 import { anything, mock, verify, when } from "ts-mockito";
 const commandInteractionMock = mock(ChatInputCommandInteraction);
-export const mockSlashCommand = (commandName: string) => {
+export const mockSlashCommand = (commandName: string, options: any = {}) => {
 	const found = new DiscordCommandRegister().commands.find(
 		(b) => b.name === commandName,
 	);
+	const optionsMock =
+		mock<
+			Omit<
+				CommandInteractionOptionResolver<CacheType>,
+				"getMessage" | "getFocused"
+			>
+		>();
+	found?.options.forEach((it) => {
+		if (options[it.toJSON().name] != null) {
+			switch (it.toJSON().type) {
+				case ApplicationCommandOptionType.Subcommand:
+				case ApplicationCommandOptionType.SubcommandGroup:
+					break;
+				case ApplicationCommandOptionType.String:
+					when(optionsMock.getString(it.toJSON().name)).thenReturn(
+						options[it.toJSON().name],
+					);
+					break;
+				case ApplicationCommandOptionType.Integer:
+					when(optionsMock.getInteger(it.toJSON().name)).thenReturn(
+						options[it.toJSON().name],
+					);
+					break;
+				case ApplicationCommandOptionType.Boolean:
+					when(optionsMock.getBoolean(it.toJSON().name)).thenReturn(
+						options[it.toJSON().name],
+					);
+					break;
+				case ApplicationCommandOptionType.User:
+				case ApplicationCommandOptionType.Channel:
+				case ApplicationCommandOptionType.Role:
+				case ApplicationCommandOptionType.Mentionable:
+				case ApplicationCommandOptionType.Number:
+				case ApplicationCommandOptionType.Attachment:
+					break;
+			}
+		}
+	});
+	when(commandInteractionMock.options).thenReturn(optionsMock);
 	when(commandInteractionMock.commandName).thenReturn(commandName);
 	when(commandInteractionMock.isChatInputCommand()).thenReturn(true);
 	when(commandInteractionMock.deferReply()).thenResolve();
