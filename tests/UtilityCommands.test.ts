9a6fa53575be9226d8ba046463677497f01d283d
y1wtt <awataki1206@gmail.com>
2024-11-26 00:30:13 +0900

    feat: impl /help test

---
+++ tests/UtilityCommands.test.ts
@@ -0,0 +1,22 @@
+import { InternalErrorMessage } from "@/src/entities/DiscordErrorMessages";
+import {
+	mockSlashCommand,
+	waitUntilReply,
+} from "@/tests/fixtures/discord.js/MockSlashCommand";
+import { TestDiscordServer } from "@/tests/fixtures/discord.js/TestDiscordServer";
+import { anything, instance, verify, when } from "ts-mockito";
+
+describe("Test UtilityCommand", () => {
+	test("Test /help category:all", async () => {
+		const commandMock = mockSlashCommand("help", {
+			category: "all",
+		});
+		const TEST_CLIENT = await TestDiscordServer.getClient();
+
+		TEST_CLIENT.emit("interactionCreate", instance(commandMock));
+		await waitUntilReply(commandMock);
+		verify(commandMock.reply(anything())).once();
+		verify(commandMock.reply("")).never();
+		verify(commandMock.reply(InternalErrorMessage)).never();
+	});
+});
