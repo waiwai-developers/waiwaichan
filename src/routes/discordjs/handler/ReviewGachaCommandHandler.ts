e2a5012b601d4eb1704071418802c042e15948a1
y1wtt <awataki1206@gmail.com>
2024-11-28 00:50:26 +0900

    feat: split review command handler

---
+++ src/routes/discordjs/handler/ReviewGachaCommandHandler.ts
@@ -0,0 +1,29 @@
+import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
+import { GithubPullRequestId } from "@/src/entities/vo/GithubPullRequestId";
+import type { IPullRequestLogic } from "@/src/logics/Interfaces/logics/IPullRequestLogic";
+import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { inject, injectable } from "inversify";
+import type { SlashCommandHandler } from "./SlashCommandHandler";
+
+@injectable()
+export class ReviewGachaCommandHandler implements SlashCommandHandler {
+	@inject(LogicTypes.PullRequestLogic)
+	private pullRequestLogic!: IPullRequestLogic;
+
+	isHandle(commandName: string): boolean {
+		return commandName === "reviewgacha";
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+	): Promise<void> {
+		await interaction.deferReply();
+		await interaction.editReply(
+			await this.pullRequestLogic.randomAssign(
+				new GithubPullRequestId(interaction.options?.getInteger("id", true)),
+				new DiscordUserId(interaction.user.id),
+			),
+		);
+	}
+}
