1f11b4f8f29526cd5c1fac80eb3795b8619ff5a3
y1wtt <awataki1206@gmail.com>
2024-11-28 00:49:55 +0900

    feat: split minecraft command handler

---
+++ src/routes/discordjs/handler/MinecraftStopCommandHandler.ts
@@ -0,0 +1,21 @@
+import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import type { IMinecraftServerLogic } from "@/src/logics/Interfaces/logics/IMinecraftServerLogic";
+import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { inject, injectable } from "inversify";
+import type { SlashCommandHandler } from "./SlashCommandHandler";
+@injectable()
+export class MinecraftStopCommandHandler implements SlashCommandHandler {
+	@inject(LogicTypes.MinecraftServerLogic)
+	private mineCraftLogic!: IMinecraftServerLogic;
+
+	isHandle(commandName: string): boolean {
+		return commandName === "minecraftstop";
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+	): Promise<void> {
+		await interaction.deferReply();
+		await interaction.editReply(await this.mineCraftLogic.stopServer());
+	}
+}
