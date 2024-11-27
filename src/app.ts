7d9cabdd4803ca00b09eeed1afa96fff574bde62
y1wtt <awataki1206@gmail.com>
2024-11-28 00:07:11 +0900

    feat: adding mysql initialize into app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import "reflect-metadata";
 import { AppConfig } from "@/src/entities/config/AppConfig";
+import { MysqlConnector } from "@/src/repositories/sequelize-mysql/MysqlConnector";
 import { DiscordServer } from "@/src/routes/discordjs/DiscordServer";
+
+// initialize DB
+new MysqlConnector();
+// start Discord Server
 await new DiscordServer().start(AppConfig.discord.token);
