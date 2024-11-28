070519b2d87fb6788ea515dcd0fc500ffd2fd010
y1wtt <awataki1206@gmail.com>
2024-11-29 02:56:31 +0900

    feat: adding date check in point test

--- tests/IntegrationTests/PointCommands.test.ts
+++ tests/IntegrationTests/PointCommands.test.ts
@@ -3,12 +3,16 @@ import { PointRepositoryImpl } from "@/src/repositories/sequelize-mysql";
 import { MysqlConnector } from "@/src/repositories/sequelize-mysql/MysqlConnector";
 import { mockReaction, waitUntilReply } from "@/tests/fixtures/discord.js/MockReaction";
 import { TestDiscordServer } from "@/tests/fixtures/discord.js/TestDiscordServer";
+import dayjs from "dayjs";
 import type { MessageReactionEventDetails } from "discord.js";
 import { anything, instance, mock, verify, when } from "ts-mockito";
 
 describe("Test Point Commands", () => {
 	test("test /point adding", async () => {
-		const { reaction, user, messageMock } = mockReaction(AppConfig.backend.pointEmoji, "1234", "5678");
+		const giverId = "1234";
+		const receiverId = "5678";
+		const creationDate = dayjs().add(1, "month").subtract(1, "second");
+		const { reaction, user, messageMock } = mockReaction(AppConfig.backend.pointEmoji, giverId, receiverId);
 		const TEST_CLIENT = await TestDiscordServer.getClient();
 		TEST_CLIENT.emit("messageReactionAdd", instance(reaction), instance(user), instance(mock<MessageReactionEventDetails>()));
 
@@ -16,6 +20,18 @@ describe("Test Point Commands", () => {
 
 		verify(messageMock.reply(anything())).once();
 		verify(messageMock.reply(`<@${instance(user).id}>さんが${AppConfig.backend.pointEmoji}スタンプを押したよ！！っ`)).once();
+
+		new MysqlConnector();
+		const res = await PointRepositoryImpl.findAll();
+		expect(res.length).toEqual(1);
+
+		expect(String(res[0].giveUserId)).toBe(giverId);
+		expect(String(res[0].receiveUserId)).toBe(receiverId);
+
+		const finishedDate = dayjs().add(1, "month").add(1, "second");
+
+		expect(creationDate.isBefore(dayjs(res[0].expiredAt))).toBe(true);
+		expect(finishedDate.isAfter(dayjs(res[0].expiredAt))).toBe(true);
 	});
 
 	test("test /point adding limit", async () => {
