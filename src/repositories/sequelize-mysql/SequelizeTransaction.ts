5dbae41eb5098131720a5ab8902c9fa206568286
y1wtt <awataki1206@gmail.com>
2024-11-29 02:10:31 +0900

    fix: adding CLS

--- src/repositories/sequelize-mysql/SequelizeTransaction.ts
+++ src/repositories/sequelize-mysql/SequelizeTransaction.ts
@@ -1,12 +1,17 @@
 import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
 import type { IDataBaseConnector } from "@/src/logics/Interfaces/repositories/database/IDataBaseConnector";
+import { createNamespace } from "cls-hooked";
 import { inject, injectable } from "inversify";
-import type { Sequelize, Transaction } from "sequelize";
+import { Sequelize, type Transaction } from "sequelize";
 
 @injectable()
 export class SequelizeTransaction implements ITransaction<Transaction> {
 	@inject(RepoTypes.DatabaseConnector)
 	declare mysql: IDataBaseConnector<Sequelize, "mysql">;
+	constructor() {
+		const namespace = createNamespace("sequelize-mysql-transactions");
+		Sequelize.useCLS(namespace);
+	}
 	async startTransaction<R>(
 		cb: (t: Transaction) => PromiseLike<R>,
 	): Promise<R> {
