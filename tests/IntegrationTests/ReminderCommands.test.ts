9ce8f50f20a524a410013a713565efb6129345cf
y1wtt <awataki1206@gmail.com>
2024-11-29 18:37:38 +0900

    feat: adding reminder scheduler test when no reminder

--- tests/IntegrationTests/ReminderCommands.test.ts
+++ tests/IntegrationTests/ReminderCommands.test.ts
@@ -314,6 +314,41 @@ describe("Test Reminder Commands", () => {
 			}, 100);
 		});
 
+		test("test reminder no post when no reminder registered", async () => {
+			const channelMock = mock<TextChannel>();
+			const mockedChannel = instance(channelMock);
+			Object.setPrototypeOf(mockedChannel, TextChannel.prototype);
+			const cacheMock = mock<Collection<Snowflake, Channel>>();
+			when(cacheMock.get(anything())).thenReturn(mockedChannel);
+			const channelManagerMock = mock<ChannelManager>();
+			when(channelManagerMock.cache).thenReturn(instance(cacheMock));
+			const clientMock = mock<Client>();
+			when(clientMock.channels).thenReturn(instance(channelManagerMock));
+			await ReminderNotifyHandler(instance(clientMock));
+			try {
+				await new Promise((resolve, reject) => {
+					const startTime = Date.now();
+					const timer = setInterval(() => {
+						try {
+							console.log("check");
+							verify(channelMock.send(anything())).atLeast(1);
+							clearInterval(timer);
+							return resolve(null);
+						} catch (_) {
+							if (Date.now() - startTime > 500) {
+								clearInterval(timer);
+								reject(new Error("Timeout: Method was not called within the time limit."));
+							}
+						}
+					}, 100);
+				});
+			} catch (e) {
+				verify(channelMock.send(anything())).never();
+				return;
+			}
+			expect("expect reach here").toBe(false);
+		});
+
 		const res = await ReminderRepositoryImpl.findAll();
 		expect(res.length).toBe(2);
 
