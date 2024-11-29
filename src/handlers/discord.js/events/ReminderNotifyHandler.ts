86f57c620e1494ead7e4333b81970c7eb2b1bef9
y1wtt <awataki1206@gmail.com>
2024-11-29 16:43:30 +0900

    feat: split ReminderNotifyHandler form scheduler

---
+++ src/handlers/discord.js/events/ReminderNotifyHandler.ts
@@ -0,0 +1,35 @@
+import {
+	RepoTypes,
+	SchedulerRepoTypes,
+} from "@/src/entities/constants/DIContainerTypes";
+import type { IReminderSchedulerRepository } from "@/src/logics/Interfaces/repositories/database/IReminderSchedulerRepository";
+import { schedulerContainer } from "@/src/scheduler.di.config";
+import { type Client, TextChannel } from "discord.js";
+
+export const ReminderNotifyHandler = async (c: Client<boolean>) => {
+	try {
+		const t = schedulerContainer.get<ITransaction<TransactionLike>>(
+			RepoTypes.Transaction,
+		);
+		await t.startTransaction(async (_t) => {
+			const reminder = schedulerContainer.get<IReminderSchedulerRepository>(
+				SchedulerRepoTypes.ReminderSchedulerRepository,
+			);
+			const remainders = await reminder.findByRemindTime();
+
+			if (remainders.length === 0) return;
+
+			for (const remainder of remainders) {
+				const channel = c.channels.cache.get(remainder.channelId.getValue());
+				if (channel != null && channel instanceof TextChannel) {
+					await channel.send(
+						`${remainder.receiveUserName.getValue()}\n${remainder.message.getValue()}`,
+					);
+				}
+				await reminder.deleteReminder(remainder.id);
+			}
+		});
+	} catch (e) {
+		console.error("Error:", e);
+	}
+};
--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,12 +1,7 @@
 import "reflect-metadata";
 import { AppConfig } from "@/src/entities/config/AppConfig";
-import {
-	RepoTypes,
-	SchedulerRepoTypes,
-} from "@/src/entities/constants/DIContainerTypes";
-import type { IReminderSchedulerRepository } from "@/src/logics/Interfaces/repositories/database/IReminderSchedulerRepository";
-import { schedulerContainer } from "@/src/scheduler.di.config";
-import { Client, GatewayIntentBits, TextChannel } from "discord.js";
+import { ReminderNotifyHandler } from "@/src/handlers/discord.js/events/ReminderNotifyHandler";
+import { Client, GatewayIntentBits } from "discord.js";
 import cron from "node-cron";
 
 const client = new Client({
@@ -16,35 +11,5 @@ const client = new Client({
 	),
 });
 
-const reminderNotifyHandler = async () => {
-	try {
-		const t = schedulerContainer.get<ITransaction<TransactionLike>>(
-			RepoTypes.Transaction,
-		);
-		await t.startTransaction(async (_t) => {
-			const reminder = schedulerContainer.get<IReminderSchedulerRepository>(
-				SchedulerRepoTypes.ReminderSchedulerRepository,
-			);
-			const remainders = await reminder.findByRemindTime();
-
-			if (remainders.length === 0) return;
-
-			for (const remainder of remainders) {
-				const channel = client.channels.cache.get(
-					remainder.channelId.getValue(),
-				);
-				if (channel != null && channel instanceof TextChannel) {
-					await channel.send(
-						`${remainder.receiveUserName.getValue()}\n${remainder.message.getValue()}`,
-					);
-				}
-				await reminder.deleteReminder(remainder.id);
-			}
-		});
-	} catch (e) {
-		console.error("Error:", e);
-	}
-};
-
-cron.schedule("* * * * *", reminderNotifyHandler);
+cron.schedule("* * * * *", () => ReminderNotifyHandler(client));
 await client.login(AppConfig.discord.token);
