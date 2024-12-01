79e43c3dba1377dadf797ae3f4e272cb8f5f076d
mamepon2580 <g.poso.2580@gmail.com>
2024-12-02 01:23:50 +0900

    fix: delete IReminderRepository import

--- src/logics/Interfaces/repositories/database/IReminderRepository.ts
+++ src/logics/Interfaces/repositories/database/IReminderRepository.ts
@@ -1,10 +1,6 @@
 import type { ReminderDto } from "@/src/entities/dto/ReminderDto";
-import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
-import type { ReceiveDiscordUserName } from "@/src/entities/vo/ReceiveDiscordUserName";
-import type { RemindTime } from "@/src/entities/vo/RemindTime";
 import type { ReminderId } from "@/src/entities/vo/ReminderId";
-import type { ReminderMessage } from "@/src/entities/vo/ReminderMessage";
 
 export interface IReminderRepository {
 	create(data: ReminderDto): Promise<boolean>;
