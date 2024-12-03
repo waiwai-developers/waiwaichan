d268809e83e43f3054e913f1be539f5ced0367c5
mamepon2580 <g.poso.2580@gmail.com>
2024-12-03 17:58:00 +0900

    add: fix user item

---
+++ migrator/datafixies/models/DatafixUserItemModel.ts
@@ -0,0 +1,41 @@
+import {
+	AutoIncrement,
+	Column,
+	DataType,
+	Model,
+	PrimaryKey,
+	Table,
+} from "sequelize-typescript";
+
+@Table({
+	tableName: "UserItems",
+	timestamps: true,
+})
+class DatafixUserItemModel extends Model {
+	@PrimaryKey
+	@AutoIncrement
+	@Column(DataType.INTEGER)
+	declare id: number;
+	@Column(DataType.BIGINT)
+	declare userId: number;
+	@Column(DataType.INTEGER)
+	declare itemId: number;
+	@Column(DataType.BOOLEAN)
+	declare status: boolean;
+	@Column(DataType.DATE)
+	declare expiredAt: Date;
+
+	async bulkUpsert(
+		data: Array<{ id: number; userId: number; itemId: number; status: boolean; expiredAt: Date }>,
+	) {
+		return await Promise.all(
+			data.map(async (d) => await DatafixUserItemModel.upsert(d)),
+		);
+	}
+	async findAll() {
+		return await DatafixUserItemModel.findAll()
+	}
+}
+
+export { DatafixUserItemModel };
+
