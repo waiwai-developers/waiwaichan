017f839fcba0c9ffee1bfcd384978de461fcf09e
mamepon2580 <g.poso.2580@gmail.com>
2024-12-06 21:37:25 +0900

    fix: migrate remove column

---
+++ migrator/migrations/20241206212900-delete-column-point.ts
@@ -0,0 +1,9 @@
+import type { Migration } from "@/migrator/umzug";
+import { DataTypes } from "sequelize";
+
+const TABLE_NAME = "Points";
+const COLUMN_NAME = "status";
+
+export const up: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().removeColumn(TABLE_NAME, COLUMN_NAME);
+};
