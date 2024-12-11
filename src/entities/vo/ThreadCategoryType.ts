9837e27adb48d0416660ebe76e2dfb26c5c90aaf
mamepon2580 <g.poso.2580@gmail.com>
2024-12-11 14:15:06 +0900

    add: vo static thread category type

--- src/entities/vo/ThreadCategoryType.ts
+++ src/entities/vo/ThreadCategoryType.ts
@@ -1,3 +1,5 @@
 import { ValueObject } from "./ValueObject";
 
-export class ThreadCategoryType extends ValueObject<number> {}
+export class ThreadCategoryType extends ValueObject<number> {
+    static CATEGORY_TYPE_CHATGPT = new ThreadCategoryType(0);
+}
