67e71ddd810b34041000d6c2e718bceeba745a5c
mamepon2580 <g.poso.2580@gmail.com>
2025-01-28 22:54:24 +0900

    add: dto

---
+++ src/entities/dto/ShiritoriDto.ts
@@ -0,0 +1,19 @@
+import type { ShiritoriWordUserId } from "@/src/entities/vo/ShiritoriWordUserId";
+import type { ShiritoriWordGuildId } from "@/src/entities/vo/ShiritoriWordGuildId";
+import type { ShiritoriWordThreadId } from "@/src/entities/vo/ShiritoriWordThreadId";
+import type { ShiritoriWordMessageId } from "@/src/entities/vo/ShiritoriWordMessageId";
+import type { ShiritoriWordReadingWord } from "@/src/entities/vo/ShiritoriWordReadingWord";
+import type { ShiritoriWordWritingWord } from "@/src/entities/vo/ShiritoriWordWritingWord";
+import type { ShiritoriWordNextMessageId } from "@/src/entities/vo/ShiritoriWordNextMessageId";
+
+export class ThreadDto {
+	constructor(
+		public userId: ShiritoriWordUserId,
+		public guildId: ShiritoriWordGuildId,
+		public threadId: ShiritoriWordThreadId,
+		public messageId: ShiritoriWordMessageId,
+		public readingWord: ShiritoriWordReadingWord,
+		public writingWord: ShiritoriWordWritingWord,
+		public nextMessageId: ShiritoriWordNextMessageId,
+	) {}
+}
