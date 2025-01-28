724c62ef4a296cc92baac97efaeab42980967423
y1wtt <awataki1206@gmail.com>
2025-01-29 00:44:31 +0900

    feat: separate Logger for scheduler

--- src/scheduler.di.config.ts
+++ src/scheduler.di.config.ts
@@ -1,7 +1,7 @@
 import { RepoTypes, SchedulerRepoTypes } from "@/src/entities/constants/DIContainerTypes";
 import type { IDataBaseConnector } from "@/src/logics/Interfaces/repositories/database/IDataBaseConnector";
 import type { IReminderSchedulerRepository } from "@/src/logics/Interfaces/repositories/database/IReminderSchedulerRepository";
-import { PinoLogger } from "@/src/repositories/logger/PinoLogger";
+import { PinoSchedulerLogger } from "@/src/repositories/logger/PinoSchedulerLogger";
 import { ReminderSchedulerRepositoryImpl } from "@/src/repositories/sequelize-mysql";
 import { MysqlSchedulerConnector } from "@/src/repositories/sequelize-mysql/MysqlSchedulerConnector";
 import { SequelizeTransaction } from "@/src/repositories/sequelize-mysql/SequelizeTransaction";
@@ -9,7 +9,7 @@ import { Container } from "inversify";
 import type { Sequelize } from "sequelize-typescript";
 
 const schedulerContainer = new Container(); // Logger
-schedulerContainer.bind<ILogger>(RepoTypes.Logger).to(PinoLogger);
+schedulerContainer.bind<ILogger>(RepoTypes.Logger).to(PinoSchedulerLogger);
 schedulerContainer.bind<IDataBaseConnector<Sequelize, "mysql">>(RepoTypes.DatabaseConnector).to(MysqlSchedulerConnector).inSingletonScope();
 schedulerContainer.bind<ITransaction<TransactionLike>>(RepoTypes.Transaction).to(SequelizeTransaction);
 schedulerContainer.bind<IReminderSchedulerRepository>(SchedulerRepoTypes.ReminderSchedulerRepository).to(ReminderSchedulerRepositoryImpl);
