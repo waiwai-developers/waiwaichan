6fe1088680e804b0794d2014de5f699e2f29989d
mamepon2580 <g.poso.2580@gmail.com>
2025-04-11 17:09:48 +0900

    add: handler candy series draw

---
+++ src/handlers/discord.js/commands/CandySeriesDrawCommandHandler.ts
@@ -0,0 +1,24 @@
+import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
+import type { SlashCommandHandler } from "@/src/handlers/discord.js/commands/SlashCommandHandler";
+import type { ICandyLogic } from "@/src/logics/Interfaces/logics/ICandyLogic";
+import type { CacheType, ChatInputCommandInteraction } from "discord.js";
+import { inject, injectable } from "inversify";
+
+@injectable()
+export class CandySeriesDrawCommandHandler implements SlashCommandHandler {
+	@inject(LogicTypes.CandyLogic)
+	private candyLogic!: ICandyLogic;
+
+	isHandle(commandName: string): boolean {
+		return commandName === "candyseriesdraw";
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+	): Promise<void> {
+		await interaction.reply(
+			await this.candyLogic.drawSeriesItem(new DiscordUserId(interaction.user.id)),
+		);
+	}
+}
