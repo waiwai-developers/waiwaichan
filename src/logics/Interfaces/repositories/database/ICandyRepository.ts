fef6a7378f0e4d63e6037331a3e0637d82876ad5
mamepon2580 <g.poso.2580@gmail.com>
2025-04-17 19:09:49 +0900

    add: interface candy repository

--- src/logics/Interfaces/repositories/database/ICandyRepository.ts
+++ src/logics/Interfaces/repositories/database/ICandyRepository.ts
@@ -1,15 +1,17 @@
 import type { CandyDto } from "@/src/entities/dto/CandyDto";
 import type { CandyCount } from "@/src/entities/vo/CandyCount";
 import type { CandyExpire } from "@/src/entities/vo/CandyExpire";
+import type { CandyCreatedAt } from "@/src/entities/vo/CandyCreatedAt";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
 import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 
 export interface ICandyRepository {
 	createCandy(data: CandyDto): Promise<boolean>;
+	bulkCreateCandy(data: CandyDto[]): Promise<boolean>;
 	candyCount(userId: DiscordUserId): Promise<CandyCount>;
 	candyExpire(userId: DiscordUserId): Promise<CandyExpire | undefined>;
-	countByToday(userId: DiscordUserId): Promise<CandyCount>;
+	countByPeriod(userId: DiscordUserId, createdAt: CandyCreatedAt ): Promise<CandyCount>;
 	findByGiverAndMessageId(
 		giver: DiscordChannelId,
 		messageId: DiscordMessageId,
