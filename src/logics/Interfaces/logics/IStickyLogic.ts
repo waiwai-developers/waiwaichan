ddafec41bc3f63517d46be253b97118c72038214
mamepon2580 <g.poso.2580@gmail.com>
2025-04-19 15:16:35 +0900

    add: update logic

--- src/logics/Interfaces/logics/IStickyLogic.ts
+++ src/logics/Interfaces/logics/IStickyLogic.ts
@@ -1,6 +1,7 @@
 import type { StickyDto } from "@/src/entities/dto/StickyDto";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
 import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
+import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 
 export interface IStickyLogic {
 	create(data: StickyDto): Promise<string>;
@@ -8,8 +9,10 @@ export interface IStickyLogic {
 		guildId: DiscordGuildId,
 		channelId: DiscordChannelId,
 	): Promise<StickyDto | undefined>;
-	delete(
+	delete(guildId: DiscordGuildId, channelId: DiscordChannelId): Promise<string>;
+	update(
 		guildId: DiscordGuildId,
 		channelId: DiscordChannelId,
+		messageId: DiscordMessageId,
 	): Promise<string>;
 }
