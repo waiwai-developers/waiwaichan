de5719b604955b575134b6a7bbb3fa3934728872
mamepon2580 <g.poso.2580@gmail.com>
2025-04-19 15:17:21 +0900

    add: interface repository

--- src/logics/Interfaces/repositories/database/IStickyRepository.ts
+++ src/logics/Interfaces/repositories/database/IStickyRepository.ts
@@ -1,6 +1,7 @@
 import type { StickyDto } from "@/src/entities/dto/StickyDto";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
 import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
+import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 
 export interface IStickyRepository {
 	create(data: StickyDto): Promise<boolean>;
@@ -11,5 +12,10 @@ export interface IStickyRepository {
 	delete(
 		guildId: DiscordGuildId,
 		channelId: DiscordChannelId,
+	): Promise<boolean>;
+	updateForMessageId(
+		guildId: DiscordGuildId,
+		channelId: DiscordChannelId,
+		messageId: DiscordMessageId,
 	): Promise<boolean>;
 }
