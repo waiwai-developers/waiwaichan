de233ca624dddf18b300f68f39d8ca271ea4b2dd
mamepon2580 <g.poso.2580@gmail.com>
2025-04-20 17:54:08 +0900

    fix: interface repository

--- src/logics/Interfaces/repositories/database/ICandyRepository.ts
+++ src/logics/Interfaces/repositories/database/ICandyRepository.ts
@@ -2,17 +2,32 @@ import type { CandyDto } from "@/src/entities/dto/CandyDto";
 import type { CandyCount } from "@/src/entities/vo/CandyCount";
 import type { CandyExpire } from "@/src/entities/vo/CandyExpire";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
+import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 
 export interface ICandyRepository {
 	createCandy(data: CandyDto): Promise<boolean>;
-	candyCount(userId: DiscordUserId): Promise<CandyCount>;
-	candyExpire(userId: DiscordUserId): Promise<CandyExpire | undefined>;
-	countByToday(userId: DiscordUserId): Promise<CandyCount>;
+	candyCount(
+		guildId: DiscordGuildId,
+		userId: DiscordUserId,
+	): Promise<CandyCount>;
+	candyExpire(
+		guildId: DiscordGuildId,
+		userId: DiscordUserId,
+	): Promise<CandyExpire | undefined>;
+	countByToday(
+		guildId: DiscordGuildId,
+		userId: DiscordUserId,
+	): Promise<CandyCount>;
 	findByGiverAndMessageId(
+		guildId: DiscordGuildId,
 		giver: DiscordChannelId,
 		messageId: DiscordMessageId,
 	): Promise<Array<CandyDto>>;
-	ConsumeCandies(userId: DiscordUserId, Candies?: CandyCount): Promise<boolean>;
+	ConsumeCandies(
+		guildId: DiscordGuildId,
+		userId: DiscordUserId,
+		Candies?: CandyCount,
+	): Promise<boolean>;
 }
--- src/logics/Interfaces/repositories/database/IUserCandyItemRepository.ts
+++ src/logics/Interfaces/repositories/database/IUserCandyItemRepository.ts
@@ -1,6 +1,7 @@
 import type { UserCandyItemDto } from "@/src/entities/dto/UserCandyItemDto";
 import type { UserCandyItemWithItemGroupByDto } from "@/src/entities/dto/UserCandyItemWithItemGroupByDto";
 import type { CandyItemId } from "@/src/entities/vo/CandyItemId";
+import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 import type { UserCandyItemCount } from "@/src/entities/vo/UserCandyItemCount";
 import type { UserCandyItemId } from "@/src/entities/vo/UserCandyItemId";
@@ -8,10 +9,12 @@ import type { UserCandyItemId } from "@/src/entities/vo/UserCandyItemId";
 export interface IUserCandyItemRepository {
 	create(data: UserCandyItemDto): Promise<UserCandyItemId>;
 	findByNotUsed(
+		guildId: DiscordGuildId,
 		userId: DiscordUserId,
 	): Promise<UserCandyItemWithItemGroupByDto[]>;
 
 	exchangeByTypeAndAmount(
+		guildId: DiscordGuildId,
 		userId: DiscordUserId,
 		type: CandyItemId,
 		amount: UserCandyItemCount,
