b597d99b6ce8d57adb557a216191b0818deaf041
mamepon2580 <g.poso.2580@gmail.com>
2025-04-20 17:50:51 +0900

    fix: dto

--- src/entities/dto/CandyDto.ts
+++ src/entities/dto/CandyDto.ts
@@ -1,9 +1,11 @@
 import type { CandyExpire } from "@/src/entities/vo/CandyExpire";
+import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 
 export class CandyDto {
 	constructor(
+		public guildId: DiscordGuildId,
 		public receiveUserId: DiscordUserId,
 		public giveUserId: DiscordUserId,
 		public messageId: DiscordMessageId,
--- src/entities/dto/UserCandyItemDto.ts
+++ src/entities/dto/UserCandyItemDto.ts
@@ -1,10 +1,12 @@
 import type { CandyItemId } from "@/src/entities/vo/CandyItemId";
+import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 import type { UserCandyItemExpire } from "@/src/entities/vo/UserCandyItemExpire";
 import type { UserCandyItemId } from "@/src/entities/vo/UserCandyItemId";
 export class UserCandyItemDto {
 	constructor(
 		public id: UserCandyItemId,
+		public guildId: DiscordGuildId,
 		public userId: DiscordUserId,
 		public itemId: CandyItemId,
 		public expiredAt: UserCandyItemExpire,
