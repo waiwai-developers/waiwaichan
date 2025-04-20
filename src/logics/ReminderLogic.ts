e4f600661bf5a9b6aeef2b98c3b063907d25fa1c
mamepon2580 <g.poso.2580@gmail.com>
2025-04-20 19:49:17 +0900

    fix: implement logic

--- src/logics/ReminderLogic.ts
+++ src/logics/ReminderLogic.ts
@@ -1,5 +1,6 @@
 import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
 import type { ReminderDto } from "@/src/entities/dto/ReminderDto";
+import { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 import type { ReminderId } from "@/src/entities/vo/ReminderId";
 import type { IReminderLogic } from "@/src/logics/Interfaces/logics/IReminderLogic";
@@ -24,9 +25,9 @@ export class ReminderLogic implements IReminderLogic {
 			return "リマインドの投稿を予約したよ！っ";
 		});
 	}
-	list(userId: DiscordUserId): Promise<string> {
+	list(guildId: DiscordGuildId, userId: DiscordUserId): Promise<string> {
 		return this.transaction.startTransaction(async () => {
-			const reminders = await this.reminderRepository.findByUserId(userId);
+			const reminders = await this.reminderRepository.findByUserId(guildId, userId);
 			if (!reminders || reminders.length <= 0) {
 				return "リマインドは予約されていないよ！っ";
 			}
@@ -39,9 +40,9 @@ export class ReminderLogic implements IReminderLogic {
 				.join("\n");
 		});
 	}
-	delete(id: ReminderId, userId: DiscordUserId): Promise<string> {
+	delete(id: ReminderId, guildId: DiscordGuildId, userId: DiscordUserId): Promise<string> {
 		return this.transaction.startTransaction(async () => {
-			const success = await this.reminderRepository.deleteReminder(id, userId);
+			const success = await this.reminderRepository.deleteReminder(id, guildId, userId);
 			if (!success) return "リマインドの予約はされていなかったよ！っ";
 			return "リマインドの予約を削除したよ！っ";
 		});
