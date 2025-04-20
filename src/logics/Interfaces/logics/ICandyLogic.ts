b36727c07c80672fce10b9c932b144c7ac0dda3e
mamepon2580 <g.poso.2580@gmail.com>
2025-04-20 17:53:05 +0900

    fix: interface logic

--- src/logics/Interfaces/logics/ICandyLogic.ts
+++ src/logics/Interfaces/logics/ICandyLogic.ts
@@ -1,20 +1,22 @@
 import type { CandyItemId } from "@/src/entities/vo/CandyItemId";
+import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 import type { DiscordMessageLink } from "@/src/entities/vo/DiscordMessageLink";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 import type { UserCandyItemCount } from "@/src/entities/vo/UserCandyItemCount";
-import type { UserCandyItemId } from "@/src/entities/vo/UserCandyItemId";
 
 export interface ICandyLogic {
-	check(userId: DiscordUserId): Promise<string>;
+	check(guildId: DiscordGuildId, userId: DiscordUserId): Promise<string>;
 	exchange(
+		guildId: DiscordGuildId,
 		userId: DiscordUserId,
 		type: CandyItemId,
 		amount: UserCandyItemCount,
 	): Promise<string>;
-	drawItem(userId: DiscordUserId): Promise<string>;
-	getItems(userId: DiscordUserId): Promise<string>;
+	drawItem(guildId: DiscordGuildId, userId: DiscordUserId): Promise<string>;
+	getItems(guildId: DiscordGuildId, userId: DiscordUserId): Promise<string>;
 	giveCandy(
+		guildId: DiscordGuildId,
 		receiver: DiscordUserId,
 		giver: DiscordUserId,
 		messageId: DiscordMessageId,
