759159863589eeef0351c1688f58300d16bf0fdd
mamepon2580 <g.poso.2580@gmail.com>
2025-04-20 19:48:39 +0900

    fix: interface logic

--- src/logics/Interfaces/logics/IReminderLogic.ts
+++ src/logics/Interfaces/logics/IReminderLogic.ts
@@ -1,9 +1,10 @@
 import type { ReminderDto } from "@/src/entities/dto/ReminderDto";
+import { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 import type { ReminderId } from "@/src/entities/vo/ReminderId";
 
 export interface IReminderLogic {
 	create(data: ReminderDto): Promise<string>;
-	list(userId: DiscordUserId): Promise<string>;
-	delete(id: ReminderId, userId: DiscordUserId): Promise<string>;
+	list(guildId: DiscordGuildId, userId: DiscordUserId): Promise<string>;
+	delete(id: ReminderId, guildId: DiscordGuildId, userId: DiscordUserId): Promise<string>;
 }
