310e80af484bc9b6dc9f0cfbfa4abfc4cc95639b
mamepon2580 <g.poso.2580@gmail.com>
2025-04-22 02:48:45 +0900

    delete: vo

--- src/logics/Interfaces/logics/ICandyLogic.ts
+++ src/logics/Interfaces/logics/ICandyLogic.ts
@@ -3,7 +3,6 @@ import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 import type { DiscordMessageLink } from "@/src/entities/vo/DiscordMessageLink";
 import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
 import type { UserCandyItemCount } from "@/src/entities/vo/UserCandyItemCount";
-import type { UserCandyItemId } from "@/src/entities/vo/UserCandyItemId";
 
 export interface ICandyLogic {
 	check(userId: DiscordUserId): Promise<string>;
