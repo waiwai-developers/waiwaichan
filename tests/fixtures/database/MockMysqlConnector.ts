21010ecec0e739e7b3ddf9df1b5fb9f3c7759803
mamepon2580 <g.poso.2580@gmail.com>
2025-04-23 23:12:42 +0900

    add: mock mysql connector

---
+++ tests/fixtures/database/MockMysqlConnector.ts
@@ -0,0 +1,42 @@
+import { GetEnvDBConfig } from "@/src/entities/config/DatabaseConfig";
+import { SequelizeLogger } from "@/src/repositories/sequelize-mysql/SequelizeLogger";
+import { MockLogger } from "../repositories/MockLogger";
+import { Sequelize } from "sequelize-typescript";
+import { CandyRepositoryImpl } from "@/src/repositories/sequelize-mysql/CandyRepositoryImpl";
+import { CandyItemRepositoryImpl } from "@/src/repositories/sequelize-mysql/CandyItemRepositoryImpl";
+import { UserCandyItemRepositoryImpl } from "@/src/repositories/sequelize-mysql/UserCandyItemRepositoryImpl";
+import { ReminderRepositoryImpl } from "@/src/repositories/sequelize-mysql/ReminderRepositoryImpl";
+import { ThreadRepositoryImpl } from "@/src/repositories/sequelize-mysql/ThreadRepositoryImpl";
+import type { Dialect } from "sequelize";
+import type { IDataBaseConnector } from "@/src/logics/Interfaces/repositories/database/IDataBaseConnector";
+
+export class MockMysqlConnector implements IDataBaseConnector<Sequelize, "mysql"> {
+    instance: Sequelize;
+    logger = new MockLogger();
+
+    constructor() {
+        const dbConfig = GetEnvDBConfig();
+        this.instance = new Sequelize(
+            dbConfig.database,
+            dbConfig.username,
+            dbConfig.password,
+            {
+                host: dbConfig.host,
+                port: dbConfig.port,
+                dialect: dbConfig.dialect as Dialect,
+                logging: (s, t) => SequelizeLogger(s, t, this.logger),
+                models: [
+                    CandyRepositoryImpl,
+                    CandyItemRepositoryImpl,
+                    UserCandyItemRepositoryImpl,
+                    ReminderRepositoryImpl,
+                    ThreadRepositoryImpl,
+                ],
+            },
+        );
+    }
+
+    getDBInstance(): Sequelize {
+        return this.instance;
+    }
+}
