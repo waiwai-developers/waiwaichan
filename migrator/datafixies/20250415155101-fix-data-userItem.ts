d057ad04f6f51c994ed609b425196af79f1e31b6
mamepon2580 <g.poso.2580@gmail.com>
2025-04-28 15:04:08 +0900

    add: datafix transaction

--- migrator/datafixies/20250415155101-fix-data-userItem.ts
+++ migrator/datafixies/20250415155101-fix-data-userItem.ts
@@ -2,28 +2,33 @@ import type { Datafix } from "@/migrator/umzug";
 import { DatafixUserItemModel } from "./models/DatafixUserItemModel";
 import { DatafixCandyModel } from "./models/DatafixCandyModel";
 import { Op, col } from "sequelize";
+import { SequelizeTransaction } from "@/src/repositories/sequelize-mysql/SequelizeTransaction";
+import type { ITransaction } from "@/src/logics/Interfaces/repositories/database/ITransaction";
 
 export const up: Datafix = async () => {
-	const userItems = await DatafixUserItemModel.findAll();
-	let candyIds: number[] = []
+	const transaction :ITransaction = new SequelizeTransaction();
+	return transaction.startTransaction(async () => {
+		const userItems = await DatafixUserItemModel.findAll();
+		let candyIds: number[] = []
 
-	for (const ui of userItems) {
-	    await DatafixCandyModel.findOne(
-			{
-				where: {
-					id: { [Op.notIn]: candyIds },
-					receiveUserId: ui.userId,
-					deletedAt: {
-						[Op.lte]: ui.createdAt,
+		for (const ui of userItems) {
+			await DatafixCandyModel.findOne(
+				{
+					where: {
+						id: { [Op.notIn]: candyIds },
+						receiveUserId: ui.userId,
+						deletedAt: {
+							[Op.lte]: ui.createdAt,
+						},
 					},
-				},
-				order: [[col("createdAt"), "DESC"]],
-			}
-		).then(async (c) => {
-			if (!c) {return}
-			await ui.update({ candyId: c.id })
-			candyIds.push(c.id)
-		})
-	}
+					order: [[col("createdAt"), "DESC"]],
+				}
+			).then(async (c) => {
+				if (!c) {return}
+				await ui.update({ candyId: c.id })
+				candyIds.push(c.id)
+			})
+		}
+		return Promise.resolve();
+	});
 };
-
