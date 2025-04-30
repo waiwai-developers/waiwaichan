086875fa35b4ed63cba07346f3ac31c0aeef9e1b
mamepon2580 <g.poso.2580@gmail.com>
2025-05-01 00:28:44 +0900

    fix: hander

--- src/handlers/discord.js/commands/TalkCommandHandler.ts
+++ src/handlers/discord.js/commands/TalkCommandHandler.ts
@@ -4,19 +4,29 @@ import { ThreadCategoryType } from "@/src/entities/vo/ThreadCategoryType";
 import { ThreadGuildId } from "@/src/entities/vo/ThreadGuildId";
 import { ThreadMessageId } from "@/src/entities/vo/ThreadMessageId";
 import { ThreadMetadataChatgpt } from "@/src/entities/vo/ThreadMetadataChatgpt";
+import { PersonalityId } from "@/src/entities/vo/PersonalityId";
+import { PersonalityCategoryId } from "@/src/entities/vo/PersonalityCategoryId";
+import { PersonalityCategoryPersonalityId } from "@/src/entities/vo/PersonalityCategoryPersonalityId";
 import type { SlashCommandHandler } from "@/src/handlers/discord.js/commands/SlashCommandHandler";
 import type { IThreadLogic } from "@/src/logics/Interfaces/logics/IThreadLogic";
+import type { IPersonalityLogic } from "@/src/logics/Interfaces/logics/IPersonalityLogic";
+import type { IPersonalityCategoryLogic } from "@/src/logics/Interfaces/logics/IPersonalityCategoryLogic";
 import type {
 	CacheType,
 	ChatInputCommandInteraction,
 	TextChannel,
 } from "discord.js";
 import { inject, injectable } from "inversify";
+import { metadata } from "@abraham/reflection";
 
 @injectable()
 export class TalkCommandHandler implements SlashCommandHandler {
 	@inject(LogicTypes.ThreadLogic)
 	private readonly threadLogic!: IThreadLogic;
+	@inject(LogicTypes.PersonalityLogic)
+	private readonly personalityLogic!: IPersonalityLogic;
+	@inject(LogicTypes.PersonalityCategoryLogic)
+	private readonly personalityCategoryLogic!: IPersonalityCategoryLogic;
 	isHandle(commandName: string): boolean {
 		return commandName === "talk";
 	}
@@ -38,24 +48,29 @@ export class TalkCommandHandler implements SlashCommandHandler {
 			return;
 		}
 
-		const title = interaction.options.getString("title", true);
+		const personality = await this.personalityLogic.find(PersonalityId.PERSONALITY_ID_WAIWAICHAN)
+		if (!personality) {
+			return;
+		}
+		const personalityCategory = await this.personalityCategoryLogic.find(new PersonalityCategoryId(interaction.options.getInteger("id", true)), new PersonalityCategoryPersonalityId(personality.id.getValue()))
+		if (!personalityCategory) {
+			return;
+		}
 
+		const title = interaction.options.getString("title", true);
 		const message = await interaction.reply({
 			content: "以下にお話する場を用意したよ！っ",
 			fetchReply: true,
 		});
+		const metadata = Object.assign(personality.personality.getValue(), personalityCategory.context.getValue())
 
 		await this.threadLogic.create(
 			new ThreadDto(
 				new ThreadGuildId(message.guildId),
 				new ThreadMessageId(message.id),
 				ThreadCategoryType.CATEGORY_TYPE_CHATGPT,
-				new ThreadMetadataChatgpt(
-					JSON.parse(
-						`{
-						}`,
-					),
-				),
+				//ここJSONの型を後で矯正する
+				new ThreadMetadataChatgpt(metadata),
 			),
 		);
 
