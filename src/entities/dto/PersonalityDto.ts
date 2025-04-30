223fb5b3f3831a7f0781d2f3d116d796dbc552e9
mamepon2580 <g.poso.2580@gmail.com>
2025-05-01 00:28:09 +0900

    fix: dto

--- src/entities/dto/PersonalityDto.ts
+++ src/entities/dto/PersonalityDto.ts
@@ -1,8 +1,10 @@
+import type { PersonalityId } from "@/src/entities/vo/PersonalityId";
 import type { PersonalityName } from "@/src/entities/vo/PersonalityName";
 import type { PersonalityPersonality } from "@/src/entities/vo/PersonalityPersonality";
 
 export class PersonalityDto {
 	constructor(
+		public id: PersonalityId,
 		public name: PersonalityName,
 		public personality: PersonalityPersonality,
 	) {}
