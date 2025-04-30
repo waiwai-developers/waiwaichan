2f1a1353933ba97cfddcdcfcbaf888f782bea777
mamepon2580 <g.poso.2580@gmail.com>
2025-04-30 23:26:06 +0900

    fix: vo

--- src/entities/vo/PersonalityId.ts
+++ src/entities/vo/PersonalityId.ts
@@ -1,5 +1,5 @@
 import { ValueObject } from "./ValueObject";
 
 export class PersonalityId extends ValueObject<number> {
-    static CATEGORY_TYPE_CHATGPT = new PersonalityId(1);
+	static CATEGORY_TYPE_CHATGPT = new PersonalityId(1);
 }
