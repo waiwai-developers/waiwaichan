1142841df29ba01cea0a61b943b8156b96f7f3e9
mamepon2580 <g.poso.2580@gmail.com>
2025-04-30 23:27:41 +0900

    add: logic

---
+++ src/logics/PersonalityCategoryLogic.ts
@@ -0,0 +1,30 @@
+import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
+import type { PersonalityCategoryDto } from "@/src/entities/dto/PersonalityCategoryDto";
+import type { PersonalityCategoryId } from "@/src/entities/vo/PersonalityCategoryId";
+import type { PersonalityCategoryPersonalityId } from "@/src/entities/vo/PersonalityCategoryPersonalityId";
+
+import type { IPersonalityCategoryLogic } from "@/src/logics/Interfaces/logics/IPersonalityCategoryLogic";
+import type { IPersonalityCategoryRepository } from "@/src/logics/Interfaces/repositories/database/IPersonalityCategoryRepository";
+
+import { inject, injectable } from "inversify";
+
+@injectable()
+export class PersonalityLogic implements IPersonalityCategoryLogic {
+	@inject(RepoTypes.ThreadRepository)
+	private readonly personalityRepository!: IPersonalityCategoryRepository;
+
+	@inject(RepoTypes.Transaction)
+	private readonly transaction!: ITransaction;
+
+	find(
+		id: PersonalityCategoryId,
+		personalityId: PersonalityCategoryPersonalityId,
+	): Promise<PersonalityCategoryDto | undefined> {
+		return this.transaction.startTransaction(async () => {
+			return await this.personalityRepository.findByIdAndPersonalityId(
+				id,
+				personalityId,
+			);
+		});
+	}
+}
