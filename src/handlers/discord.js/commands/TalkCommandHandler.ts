1274306bf65882713e1c544247e916a5a7893602
mamepon2580 <g.poso.2580@gmail.com>
2025-05-01 10:40:24 +0900

    fix: talk hander

--- src/handlers/discord.js/commands/TalkCommandHandler.ts
+++ src/handlers/discord.js/commands/TalkCommandHandler.ts
@@ -1,23 +1,23 @@
 import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
 import { ThreadDto } from "@/src/entities/dto/ThreadDto";
+import { PersonalityCategoryId } from "@/src/entities/vo/PersonalityCategoryId";
+import { PersonalityCategoryPersonalityId } from "@/src/entities/vo/PersonalityCategoryPersonalityId";
+import { PersonalityId } from "@/src/entities/vo/PersonalityId";
 import { ThreadCategoryType } from "@/src/entities/vo/ThreadCategoryType";
 import { ThreadGuildId } from "@/src/entities/vo/ThreadGuildId";
 import { ThreadMessageId } from "@/src/entities/vo/ThreadMessageId";
 import { ThreadMetadataChatgpt } from "@/src/entities/vo/ThreadMetadataChatgpt";
-import { PersonalityId } from "@/src/entities/vo/PersonalityId";
-import { PersonalityCategoryId } from "@/src/entities/vo/PersonalityCategoryId";
-import { PersonalityCategoryPersonalityId } from "@/src/entities/vo/PersonalityCategoryPersonalityId";
 import type { SlashCommandHandler } from "@/src/handlers/discord.js/commands/SlashCommandHandler";
-import type { IThreadLogic } from "@/src/logics/Interfaces/logics/IThreadLogic";
-import type { IPersonalityLogic } from "@/src/logics/Interfaces/logics/IPersonalityLogic";
 import type { IPersonalityCategoryLogic } from "@/src/logics/Interfaces/logics/IPersonalityCategoryLogic";
+import type { IPersonalityLogic } from "@/src/logics/Interfaces/logics/IPersonalityLogic";
+import type { IThreadLogic } from "@/src/logics/Interfaces/logics/IThreadLogic";
+import { metadata } from "@abraham/reflection";
 import type {
 	CacheType,
 	ChatInputCommandInteraction,
 	TextChannel,
 } from "discord.js";
 import { inject, injectable } from "inversify";
-import { metadata } from "@abraham/reflection";
 
 @injectable()
 export class TalkCommandHandler implements SlashCommandHandler {
@@ -48,11 +48,16 @@ export class TalkCommandHandler implements SlashCommandHandler {
 			return;
 		}
 
-		const personality = await this.personalityLogic.find(PersonalityId.PERSONALITY_ID_WAIWAICHAN)
+		const personality = await this.personalityLogic.find(
+			PersonalityId.PERSONALITY_ID_WAIWAICHAN,
+		);
 		if (!personality) {
 			return;
 		}
-		const personalityCategory = await this.personalityCategoryLogic.find(new PersonalityCategoryId(interaction.options.getInteger("id", true)), new PersonalityCategoryPersonalityId(personality.id.getValue()))
+		const personalityCategory = await this.personalityCategoryLogic.find(
+			new PersonalityCategoryId(interaction.options.getInteger("id", true)),
+			new PersonalityCategoryPersonalityId(personality.id.getValue()),
+		);
 		if (!personalityCategory) {
 			return;
 		}
@@ -62,7 +67,10 @@ export class TalkCommandHandler implements SlashCommandHandler {
 			content: "以下にお話する場を用意したよ！っ",
 			fetchReply: true,
 		});
-		const metadata = Object.assign(personality.personality.getValue(), personalityCategory.context.getValue())
+		const metadata = Object.assign(
+			personality.personality.getValue(),
+			personalityCategory.context.getValue(),
+		);
 
 		await this.threadLogic.create(
 			new ThreadDto(
