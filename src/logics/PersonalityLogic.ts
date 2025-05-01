4b34c7011d2705913e813d626a99677f123f273b
mamepon2580 <g.poso.2580@gmail.com>
2025-05-01 11:43:29 +0900

    fix: personality repository

--- src/logics/PersonalityLogic.ts
+++ src/logics/PersonalityLogic.ts
@@ -3,11 +3,12 @@ import type { PersonalityDto } from "@/src/entities/dto/PersonalityDto";
 import type { PersonalityId } from "@/src/entities/vo/PersonalityId";
 import type { IPersonalityLogic } from "@/src/logics/Interfaces/logics/IPersonalityLogic";
 import type { IPersonalityRepository } from "@/src/logics/Interfaces/repositories/database/IPersonalityRepository";
+import type { ITransaction } from "@/src/logics/Interfaces/repositories/database/ITransaction";
 import { inject, injectable } from "inversify";
 
 @injectable()
 export class PersonalityLogic implements IPersonalityLogic {
-	@inject(RepoTypes.ThreadRepository)
+	@inject(RepoTypes.PersonalityRepository)
 	private readonly personalityRepository!: IPersonalityRepository;
 
 	@inject(RepoTypes.Transaction)
