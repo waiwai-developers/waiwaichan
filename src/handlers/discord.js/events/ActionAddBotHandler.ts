34bdd234f55a3ab1613da0a2896851783c967be6
mamepon2580 <g.poso.2580@gmail.com>
2025-05-11 11:23:45 +0900

    fix: handler

--- src/handlers/discord.js/events/ActionAddBotHandler.ts
+++ src/handlers/discord.js/events/ActionAddBotHandler.ts
@@ -1,19 +1,33 @@
 import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
+import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import { ActionDto } from "@/src/entities/dto/ActionDto";
+import { CommunityCategoryType } from "@/src/entities/vo/CommunityCategoryType";
+import { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
 import type { DiscordEventHandler } from "@/src/handlers/discord.js/events/DiscordEventHandler";
+import type { IActionLogic } from "@/src/logics/Interfaces/logics/IActionLogic";
 import type { ILogger } from "@/src/logics/Interfaces/repositories/logger/ILogger";
-import { inject, injectable } from "inversify";
 import type { Guild } from "discord.js";
+import { inject, injectable } from "inversify";
 
 @injectable()
-export class ActionAddBotHandler
-	implements DiscordEventHandler<Guild>
-{
+export class ActionAddBotHandler implements DiscordEventHandler<Guild> {
 	@inject(RepoTypes.Logger)
 	private readonly logger!: ILogger;
 
+	@inject(LogicTypes.ActionLogic)
+	private readonly ActionLogic!: IActionLogic;
+
 	async handle(guild: Guild): Promise<void> {
 		try {
-			this.logger.info(`ActionAddBotHandler: Bot was added to guild ${guild.id}`);
+			this.logger.info(
+				`ActionAddBotHandler: Bot was added to guild ${guild.id}`,
+			);
+			await this.ActionLogic.create(
+				new ActionDto(
+					CommunityCategoryType.Discord,
+					new CommunityClientId(guild.id),
+				),
+			);
 		} catch (error) {
 			this.logger.error(`ActionAddBotHandler error: ${error}`);
 		}
