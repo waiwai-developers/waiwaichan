2a46577caa25e1e7b9abbda5f1b9b33c47a37bbe
mamepon2580 <g.poso.2580@gmail.com>
2025-05-11 11:23:58 +0900

    fix: ilogic

---
+++ src/logics/Interfaces/logics/IActionLogic.ts
@@ -0,0 +1,8 @@
+import type { ActionDto } from "@/src/entities/dto/ActionDto";
+// import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
+// import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
+// import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
+
+export interface IActionLogic {
+	create(data: ActionDto): Promise<boolean>;
+}
