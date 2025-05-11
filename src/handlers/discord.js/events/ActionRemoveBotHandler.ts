a38e0adee67d620ee05dbccdaa50e6622c351c32
mamepon2580 <g.poso.2580@gmail.com>
2025-05-11 12:22:43 +0900

    add: remove handler

---
+++ src/handlers/discord.js/events/ActionRemoveBotHandler.ts
@@ -0,0 +1,35 @@
+import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
+import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import { ActionDto } from "@/src/entities/dto/ActionDto";
+import { CommunityCategoryType } from "@/src/entities/vo/CommunityCategoryType";
+import { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
+import type { DiscordEventHandler } from "@/src/handlers/discord.js/events/DiscordEventHandler";
+import type { IActionLogic } from "@/src/logics/Interfaces/logics/IActionLogic";
+import type { ILogger } from "@/src/logics/Interfaces/repositories/logger/ILogger";
+import type { Guild } from "discord.js";
+import { inject, injectable } from "inversify";
+
+@injectable()
+export class ActionRemoveBotHandler implements DiscordEventHandler<Guild> {
+	@inject(RepoTypes.Logger)
+	private readonly logger!: ILogger;
+
+	@inject(LogicTypes.ActionLogic)
+	private readonly ActionLogic!: IActionLogic;
+
+	async handle(guild: Guild): Promise<void> {
+		try {
+			this.logger.info(
+				`ActionRemoveBotHandler: Bot was added to guild ${guild.id}`,
+			);
+			await this.ActionLogic.delete(
+				new ActionDto(
+					CommunityCategoryType.Discord,
+					new CommunityClientId(guild.id),
+				),
+			);
+		} catch (error) {
+			this.logger.error(`ActionRemoveBotHandler error: ${error}`);
+		}
+	}
+}
