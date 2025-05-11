a2588279cb21a9027f7f713f4418bb247bfa788c
mamepon2580 <g.poso.2580@gmail.com>
2025-05-11 11:25:08 +0900

    fix: action logic

---
+++ src/logics/ActionLogic.ts
@@ -0,0 +1,21 @@
+import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
+import type { ActionDto } from "@/src/entities/dto/ActionDto";
+import type { IActionLogic } from "@/src/logics/Interfaces/logics/IActionLogic";
+import type { IActionRepository } from "@/src/logics/Interfaces/repositories/database/IActionRepository";
+import type { ITransaction } from "@/src/logics/Interfaces/repositories/database/ITransaction";
+import { inject, injectable } from "inversify";
+
+@injectable()
+export class ActionLogic implements IActionLogic {
+	@inject(RepoTypes.ActionRepository)
+	private readonly ActionRepository!: IActionRepository;
+
+	@inject(RepoTypes.Transaction)
+	private readonly transaction!: ITransaction;
+
+	async create(data: ActionDto): Promise<boolean> {
+		return this.transaction.startTransaction(async () => {
+			return await this.ActionRepository.create(data);
+		});
+	}
+}
