9b4ce052a027efa0e6ebe0262ab73257cd5d165b
mamepon2580 <g.poso.2580@gmail.com>
2025-05-12 13:09:30 +0900

    delete: comment and name

--- src/logics/Interfaces/logics/ICommunityLogic.ts
+++ src/logics/Interfaces/logics/ICommunityLogic.ts
@@ -1,9 +1,6 @@
-import type { ActionDto } from "@/src/entities/dto/ActionDto";
-// import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
-// import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
-// import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
+import type { CommunityDto } from "@/src/entities/dto/CommunityDto";
 
 export interface ICommunityLogic {
-	create(data: ActionDto): Promise<boolean>;
-	delete(data: ActionDto): Promise<boolean>;
+	create(data: CommunityDto): Promise<boolean>;
+	delete(data: CommunityDto): Promise<boolean>;
 }
