97105305dd2995564880aef3c622897a493b16cb
mamepon2580 <g.poso.2580@gmail.com>
2025-05-12 18:36:56 +0900

    fix: logic

--- src/logics/Interfaces/logics/ICommunityLogic.ts
+++ src/logics/Interfaces/logics/ICommunityLogic.ts
@@ -1,6 +1,8 @@
 import type { CommunityDto } from "@/src/entities/dto/CommunityDto";
+import type { CommunityId } from "@/src/entities/vo/CommunityId";
 
 export interface ICommunityLogic {
 	create(data: CommunityDto): Promise<boolean>;
 	delete(data: CommunityDto): Promise<boolean>;
+	getId(data: CommunityDto): Promise<CommunityId | undefined>;
 }
--- src/logics/Interfaces/logics/IUserLogic.ts
+++ src/logics/Interfaces/logics/IUserLogic.ts
@@ -1,6 +1,11 @@
 import type { UserDto } from "@/src/entities/dto/UserDto";
+import type { UserCategoryType } from "@/src/entities/vo/UserCategoryType";
+import type { UserClientId } from "@/src/entities/vo/UserClientId";
 
 export interface IUserLogic {
 	create(data: UserDto): Promise<boolean>;
-	delete(data: UserDto): Promise<boolean>;
+	delete(
+		categoryType: UserCategoryType,
+		clientId: UserClientId,
+	): Promise<boolean>;
 }
