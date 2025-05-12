130a09b9a78e0e6dad72a6779e1e5673a79157a2
mamepon2580 <g.poso.2580@gmail.com>
2025-05-12 13:06:40 +0900

    fix: dto

--- src/entities/dto/ActionDto.ts
+++
@@ -1,9 +0,0 @@
-import type { CommunityCategoryType } from "@/src/entities/vo/CommunityCategoryType";
-import type { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
-
-export class ActionDto {
-	constructor(
-		public categoryType: CommunityCategoryType,
-		public clientId: CommunityClientId,
-	) {}
-}
