4bc5cb2ccd45d3b691706f9633a7165c829d6da8
mamepon2580 <g.poso.2580@gmail.com>
2025-05-12 13:11:39 +0900

    add: repository

---
+++ src/repositories/sequelize-mysql/UserRepositoryImpl.ts
@@ -0,0 +1,56 @@
+import { UserDto } from "@/src/entities/dto/UserDto";
+import { UserCategoryType } from "@/src/entities/vo/UserCategoryType";
+import { UserClientId } from "@/src/entities/vo/UserClientId";
+import { UserCommunityClientId } from "@/src/entities/vo/UserCommunityClientId";
+import type { IUserRepository } from "@/src/logics/Interfaces/repositories/database/IUserRepository";
+import { injectable } from "inversify";
+import {
+	AutoIncrement,
+	Column,
+	DataType,
+	Model,
+	PrimaryKey,
+	Table,
+} from "sequelize-typescript";
+
+@injectable()
+@Table({
+	tableName: "Users",
+	timestamps: true,
+	paranoid: true,
+})
+class UserRepositoryImpl extends Model implements IUserRepository {
+	@PrimaryKey
+	@AutoIncrement
+	@Column(DataType.INTEGER)
+	declare id: number;
+	@Column(DataType.INTEGER)
+	declare categoryType: number;
+	@Column(DataType.STRING)
+	declare clientId: string;
+
+	async create(data: UserDto): Promise<boolean> {
+		return UserRepositoryImpl.create({
+			categoryType: data.categoryType.getValue(),
+			clientId: data.clientId.getValue(),
+		}).then((res) => !!res);
+	}
+
+	async delete(data: UserDto): Promise<boolean> {
+		return UserRepositoryImpl.destroy({
+			where: {
+				categoryType: data.categoryType.getValue(),
+				clientId: data.clientId.getValue(),
+			},
+		}).then((res) => !!res);
+	}
+
+	toDto(): UserDto {
+		return new UserDto(
+			new UserCategoryType(this.categoryType),
+			new UserClientId(this.clientId),
+			new UserCommunityClientId(this.clientId),
+		);
+	}
+}
+export { UserRepositoryImpl };
