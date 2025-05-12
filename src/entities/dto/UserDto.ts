21c5ab0c2cf30f137f057844442ccf2f62adac30
mamepon2580 <g.poso.2580@gmail.com>
2025-05-12 15:24:24 +0900

    fix: dto

--- src/entities/dto/UserDto.ts
+++ src/entities/dto/UserDto.ts
@@ -1,11 +1,11 @@
 import type { UserCategoryType } from "@/src/entities/vo/UserCategoryType";
 import type { UserClientId } from "@/src/entities/vo/UserClientId";
-import type { UserCommunityClientId } from "@/src/entities/vo/UserCommunityClientId";
+import type { UserCommunityId } from "@/src/entities/vo/UserCommunityId";
 
 export class UserDto {
 	constructor(
 		public categoryType: UserCategoryType,
 		public clientId: UserClientId,
-		public clientCommunityId: UserCommunityClientId,
+		public clientCommunityId: UserCommunityId,
 	) {}
 }
