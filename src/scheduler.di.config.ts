2e01953945c4982f10a05e8d5dffd1f574e18599
mamepon2580 <g.poso.2580@gmail.com>
2025-05-14 23:12:23 +0900

    fix: di config

--- src/scheduler.di.config.ts
+++ src/scheduler.di.config.ts
@@ -1,8 +1,10 @@
 import { RepoTypes, SchedulerRepoTypes } from "@/src/entities/constants/DIContainerTypes";
 import type { IDataBaseConnector } from "@/src/logics/Interfaces/repositories/database/IDataBaseConnector";
+import type { IDataDeletionCircular } from "@/src/logics/Interfaces/repositories/database/IDataDeletionCircular";
 import type { IReminderSchedulerRepository } from "@/src/logics/Interfaces/repositories/database/IReminderSchedulerRepository";
 import { PinoSchedulerLogger } from "@/src/repositories/logger/PinoSchedulerLogger";
 import { ReminderSchedulerRepositoryImpl } from "@/src/repositories/sequelize-mysql";
+import { DataDeletionCircularImpl } from "@/src/repositories/sequelize-mysql/DataDeletionCircularImpl";
 import { MysqlSchedulerConnector } from "@/src/repositories/sequelize-mysql/MysqlSchedulerConnector";
 import { SequelizeTransaction } from "@/src/repositories/sequelize-mysql/SequelizeTransaction";
 import { Container } from "inversify";
@@ -15,5 +17,6 @@ schedulerContainer.bind<ILogger>(RepoTypes.Logger).to(PinoSchedulerLogger);
 schedulerContainer.bind<IDataBaseConnector<Sequelize, "mysql">>(RepoTypes.DatabaseConnector).to(MysqlSchedulerConnector).inSingletonScope();
 schedulerContainer.bind<ITransaction>(RepoTypes.Transaction).to(SequelizeTransaction);
 schedulerContainer.bind<IReminderSchedulerRepository>(SchedulerRepoTypes.ReminderSchedulerRepository).to(ReminderSchedulerRepositoryImpl);
+schedulerContainer.bind<IDataDeletionCircular>(RepoTypes.DataDeletionCircular).to(DataDeletionCircularImpl);
 
 export { schedulerContainer };
