4dc36c94c36b3f2a4bac27032a6675fad78a58f9
mamepon2580 <g.poso.2580@gmail.com>
2025-05-15 01:07:49 +0900

    fix: handler

--- src/handlers/discord.js/events/CommunityAndUserDeleteHandler.ts
+++ src/handlers/discord.js/events/CommunityAndUserDeleteHandler.ts
@@ -11,101 +11,87 @@ import type { IDataDeletionCircular } from "@/src/logics/Interfaces/repositories
 import type { ITransaction } from "@/src/logics/Interfaces/repositories/database/ITransaction";
 import { schedulerContainer } from "@/src/scheduler.di.config";
 import type { Client } from "discord.js";
-
 import type { ICommunityLogic } from "@/src/logics/Interfaces/logics/ICommunityLogic";
 import type { IUserLogic } from "@/src/logics/Interfaces/logics/IUserLogic";
-import type { ILogger } from "@/src/logics/Interfaces/repositories/logger/ILogger";
-import { inject, injectable } from "inversify";
-import type { DiscordEventHandler } from "./DiscordEventHandler";
 
-@injectable()
-export class CommunityAndUserDeleteHandler
-	implements DiscordEventHandler<Client<boolean>>
-{
-	@inject(LogicTypes.CommunityLogic)
-	private readonly CommunityLogic!: ICommunityLogic;
+export const CommunityAndUserDeleteHandler = async (c: Client<boolean>) => {
+	const t = schedulerContainer.get<ITransaction>(RepoTypes.Transaction);
+	const communityLogic = schedulerContainer.get<ICommunityLogic>(LogicTypes.CommunityLogic);
+	const userLogic = schedulerContainer.get<IUserLogic>(LogicTypes.UserLogic);
+	const dataDeletionCircular = schedulerContainer.get<IDataDeletionCircular>(RepoTypes.DataDeletionCircular);
 
-	@inject(LogicTypes.UserLogic)
-	private readonly UserLogic!: IUserLogic;
+	await t.startTransaction(async () => {
+		const guilds = c.guilds.cache;
 
-	@inject(RepoTypes.DataDeletionCircular)
-	private readonly DataDeletionCircular!: IDataDeletionCircular;
+		//Guildに存在しないUserの削除
+		for (const g of guilds.values()) {
+			const guild = await c.guilds.fetch(g.id);
+			const members = await guild.members.fetch();
 
-	async handle(c: Client<boolean>): Promise<void> {
-		const t = schedulerContainer.get<ITransaction>(RepoTypes.Transaction);
-		await t.startTransaction(async () => {
-			const guilds = c.guilds.cache;
-
-			//Guildに存在しないUserの削除
-			for (const g of guilds.values()) {
-				const guild = await c.guilds.fetch(g.id);
-				const members = await guild.members.fetch();
-
-				//GuildのCommunityIdの取得
-				const communityId = await this.CommunityLogic.getId(
-					new CommunityDto(
-						CommunityCategoryType.Discord,
-						new CommunityClientId(BigInt(guild.id)),
-					),
-				);
-				if (communityId == null) {
-					continue;
-				}
-
-				//MemberのclientIdの配列の取得
-				const memberIds: UserClientId[] = [];
-				members.forEach((member) => {
-					memberIds.push(new UserClientId(BigInt(member.user.id)));
-				});
-				if (memberIds.length === 0) {
-					continue;
-				}
-
-				//Userの削除
-				await this.UserLogic.deleteByCommunityIdAndClientIds(
-					communityId,
-					memberIds,
-				);
-			}
-
-			//Botが所属してないCommunityとCommunityのUser削除
-			const communityClientIds = await this.CommunityLogic.getNotExistClientId(
-				CommunityCategoryType.Discord,
-				guilds.map((g) => new CommunityClientId(BigInt(g.id))),
+			//GuildのCommunityIdの取得
+			const communityId = await communityLogic.getId(
+				new CommunityDto(
+					CommunityCategoryType.Discord,
+					new CommunityClientId(BigInt(guild.id)),
+				),
 			);
-			for (const cc of communityClientIds) {
-				const communityId = await this.CommunityLogic.getId(
-					new CommunityDto(CommunityCategoryType.Discord, cc),
-				);
-				if (communityId == null) {
-					continue;
-				}
-
-				//Userの削除
-				await this.UserLogic.deletebyCommunityId(
-					new UserCommunityId(communityId.getValue()),
-				);
-
-				//Communityの削除
-				await this.CommunityLogic.delete(
-					new CommunityDto(CommunityCategoryType.Discord, cc),
-				);
+			if (communityId == null) {
+				continue;
 			}
 
-			//削除されたUserに関連するデータの削除
-			const userIds = await this.UserLogic.findByBatchStatusAndDeletedAt();
-			for (const u of userIds) {
-				await this.DataDeletionCircular.deleteRecordInRelatedTableUserId(u);
+			//MemberのclientIdの配列の取得
+			const memberIds: UserClientId[] = [];
+			members.forEach((member) => {
+				memberIds.push(new UserClientId(BigInt(member.user.id)));
+			});
+			if (memberIds.length === 0) {
+				continue;
 			}
 
-			//削除されたにCommunity関連するデータの削除
-			const communityIds =
-				await this.CommunityLogic.findByBatchStatusAndDeletedAt();
-			for (const c of communityIds) {
-				await this.DataDeletionCircular.deleteRecordInRelatedTableCommunityId(
-					c,
-				);
+			//Userの削除
+			await userLogic.deleteByCommunityIdAndClientIds(
+				communityId,
+				memberIds,
+			);
+		}
+
+		//Botが所属してないCommunityとCommunityのUser削除
+		const communityClientIds = await communityLogic.getNotExistClientId(
+			CommunityCategoryType.Discord,
+			guilds.map((g) => new CommunityClientId(BigInt(g.id))),
+		);
+		for (const cc of communityClientIds) {
+			const communityId = await communityLogic.getId(
+				new CommunityDto(CommunityCategoryType.Discord, cc),
+			);
+			if (communityId == null) {
+				continue;
 			}
-		});
-	}
-}
+
+			//Userの削除
+			await userLogic.deletebyCommunityId(
+				new UserCommunityId(communityId.getValue()),
+			);
+
+			//Communityの削除
+			await communityLogic.delete(
+				new CommunityDto(CommunityCategoryType.Discord, cc),
+			);
+		}
+
+		//削除されたUserに関連するデータの削除
+		const userIds = await userLogic.findByBatchStatusAndDeletedAt();
+		for (const u of userIds) {
+			await dataDeletionCircular.deleteRecordInRelatedTableUserId(u);
+		}
+
+		//削除されたにCommunity関連するデータの削除
+		const communityIds =
+			await communityLogic.findByBatchStatusAndDeletedAt();
+		for (const c of communityIds) {
+			await dataDeletionCircular.deleteRecordInRelatedTableCommunityId(
+				c,
+			);
+		}
+	});
+};
