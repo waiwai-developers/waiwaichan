40179decf1164e3a26fddeef9f84571de705f92d
mamepon2580 <g.poso.2580@gmail.com>
2025-05-15 21:20:12 +0900

    delete: debug code

--- migrator/datafixies/20250515150001-fix-data-reminder-userId.ts
+++ migrator/datafixies/20250515150001-fix-data-reminder-userId.ts
@@ -3,28 +3,6 @@ import { DatafixReminderModel } from "./models/DatafixReminderModel";
 import { DatafixUserModel } from "./models/DatafixUserModel";
 import { UserCategoryType } from "@/src/entities/vo/UserCategoryType";
 
-
-// for (const community of communities) {
-// 	try {
-// 		console.log(`processing community guild: ${community.clientId}`);
-// 		const guild = await client.guilds.fetch(community.clientId);
-// 		const members = await guild.members.fetch();
-
-// 		await DatafixUserModel.bulkCreate(
-// 			members.map((m) => ({
-// 				categoryType: UserCategoryType.Discord.getValue(),
-// 				clientId: BigInt(m.id),
-// 				communityId: community.id,
-// 				batchStatus: UserBatchStatus.Yet.getValue()
-// 			}))
-// 		);
-// 		console.log(`successfully created ${members.size} users for community guildId: ${community.clientId}`);
-// 	} catch (error: any) {
-// 		console.error(`error processing community guildId: ${community.clientId}: ${error.message}`);
-// 		continue;
-// 	}
-
-
 export const up: Datafix = async () => {
 	try {
 		const reminders = await DatafixReminderModel.findAll()
