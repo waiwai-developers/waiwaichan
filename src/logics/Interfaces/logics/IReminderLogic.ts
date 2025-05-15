f24abd38eca806f0a21001bf06bead8bd7aa5d96
mamepon2580 <g.poso.2580@gmail.com>
2025-05-15 20:12:51 +0900

    fix: logic

--- src/logics/Interfaces/logics/IReminderLogic.ts
+++ src/logics/Interfaces/logics/IReminderLogic.ts
@@ -1,14 +1,14 @@
 import type { ReminderDto } from "@/src/entities/dto/ReminderDto";
-import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
-import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
+import type { CommunityId } from "@/src/entities/vo/CommunityId";
+import type { UserId } from "@/src/entities/vo/UserId";
 import type { ReminderId } from "@/src/entities/vo/ReminderId";
 
 export interface IReminderLogic {
 	create(data: ReminderDto): Promise<string>;
-	list(guildId: DiscordGuildId, userId: DiscordUserId): Promise<string>;
+	list(communityId: CommunityId, userId: UserId): Promise<string>;
 	delete(
 		id: ReminderId,
-		guildId: DiscordGuildId,
-		userId: DiscordUserId,
+		communityId: CommunityId,
+		userId: UserId,
 	): Promise<string>;
 }
