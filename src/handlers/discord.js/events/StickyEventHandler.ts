904d0c4f9f308d2ab150fd80cf1a481a7fa13461
mamepon2580 <g.poso.2580@gmail.com>
2025-05-16 10:17:53 +0900

    fix: event handler

--- src/handlers/discord.js/events/StickyEventHandler.ts
+++ src/handlers/discord.js/events/StickyEventHandler.ts
@@ -1,9 +1,13 @@
 import { AppConfig } from "@/src/entities/config/AppConfig";
 import { LogicTypes } from "@/src/entities/constants/DIContainerTypes";
+import { CommunityDto } from "@/src/entities/dto/CommunityDto";
+import { CommunityCategoryType } from "@/src/entities/vo/CommunityCategoryType";
+import { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
+import { CommunityId } from "@/src/entities/vo/CommunityId";
 import { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
-import { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
 import type { DiscordEventHandler } from "@/src/handlers/discord.js/events/DiscordEventHandler";
+import { ICommunityLogic } from "@/src/logics/Interfaces/logics/ICommunityLogic";
 import type { IStickyLogic } from "@/src/logics/Interfaces/logics/IStickyLogic";
 import type { Message } from "discord.js";
 import { TextChannel } from "discord.js";
@@ -14,13 +18,26 @@ export class StickyEventHandler implements DiscordEventHandler<Message> {
 	@inject(LogicTypes.StickyLogic)
 	private readonly stickyLogic!: IStickyLogic;
 
+	@inject(LogicTypes.CommunityLogic)
+	private readonly CommunityLogic!: ICommunityLogic;
+
 	async handle(message: Message) {
 		if (message.author.bot && message.author.id !== AppConfig.discord.clientId)
 			return;
 		if (message.channel.isThread()) return;
 
+		if (!message.guildId) return;
+
+		const communityId = await this.CommunityLogic.getId(
+			new CommunityDto(
+				CommunityCategoryType.Discord,
+				new CommunityClientId(BigInt(message.guildId))
+			)
+		)
+		if (communityId == null) return;
+
 		const sticky = await this.stickyLogic.find(
-			new DiscordGuildId(message.guildId),
+			communityId,
 			new DiscordChannelId(message.channelId),
 		);
 		if (sticky === undefined) return;
@@ -40,8 +57,18 @@ export class StickyEventHandler implements DiscordEventHandler<Message> {
 		const stickyNewMessage = await channel.send(sticky.message.getValue());
 		if (!stickyNewMessage) return;
 
+		if (!stickyNewMessage.guildId) return;
+
+		const newCommunityId = await this.CommunityLogic.getId(
+			new CommunityDto(
+				CommunityCategoryType.Discord,
+				new CommunityClientId(BigInt(stickyNewMessage.guildId))
+			)
+		)
+		if (newCommunityId == null) return;
+
 		await this.stickyLogic.update(
-			new DiscordGuildId(stickyNewMessage.guildId),
+			newCommunityId,
 			new DiscordChannelId(stickyNewMessage.channelId),
 			new DiscordMessageId(stickyNewMessage.id),
 		);
