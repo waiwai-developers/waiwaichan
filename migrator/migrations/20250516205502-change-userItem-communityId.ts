ae739ebc2e2dc81eb48d3ec565dfaacc75a269f0
mamepon2580 <g.poso.2580@gmail.com>
2025-05-16 23:05:51 +0900

    add: migrate

---
+++ migrator/migrations/20250516205502-change-userItem-communityId.ts
@@ -0,0 +1,20 @@
+import type { Migration } from "@/migrator/umzug";
+import { DataTypes } from "sequelize";
+
+const TABLE_NAME = "UserItems";
+const COLUMN_NAME_1 = "communityId";
+const COLUMN_NAME_2 = "guildId";
+
+export const up: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().addColumn(TABLE_NAME, COLUMN_NAME_1, {
+		type: DataTypes.INTEGER,
+	});
+	await sequelize.getQueryInterface().removeColumn(TABLE_NAME, COLUMN_NAME_2);
+};
+
+export const down: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().addColumn(TABLE_NAME, COLUMN_NAME_2, {
+		type: DataTypes.BIGINT,
+	});
+	await sequelize.getQueryInterface().removeColumn(TABLE_NAME, COLUMN_NAME_1);
+};
