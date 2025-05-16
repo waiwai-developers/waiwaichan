6fb39af317ca832b69af6d4e6613b479253cd029
mamepon2580 <g.poso.2580@gmail.com>
2025-05-16 13:06:40 +0900

    fix: logger

--- src/repositories/sequelize-mysql/SequelizeLogger.ts
+++ src/repositories/sequelize-mysql/SequelizeLogger.ts
@@ -1,8 +1,21 @@
+interface ILogger {
+	trace(msg: string): void;
+	debug(msg: string): void;
+	info(msg: string): void;
+	error(msg: string): void;
+	fatal(msg: string): void;
+}
+
 export const SequelizeLogger = (
 	sql: string,
 	timing: number | undefined,
-	logger: ILogger,
+	logger?: ILogger,
 ) => {
+	if (!logger) {
+		console.log(sql);
+		return;
+	}
+
 	// @ts-ignore
 	if (typeof timing === "object" && timing?.bind) {
 		//@ts-ignore
