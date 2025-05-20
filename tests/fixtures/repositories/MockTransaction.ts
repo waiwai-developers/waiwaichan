8488f22c1e7e01d41c82c8ea5ca533c04618a255
mamepon2580 <g.poso.2580@gmail.com>
2025-05-20 11:19:08 +0900

    delete mock logger and transaction

--- tests/fixtures/repositories/MockTransaction.ts
+++
@@ -1,7 +0,0 @@
-import type { ITransaction } from "@/src/logics/Interfaces/repositories/database/ITransaction";
-
-export class MockTransaction implements ITransaction {
-    async startTransaction<R>(cb: () => PromiseLike<R>): Promise<R> {
-        return await cb();
-    }
-}
