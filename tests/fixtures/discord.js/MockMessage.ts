9fb0a82250433b9be679fe772f7a6fbc6ad9a9ff
mamepon2580 <g.poso.2580@gmail.com>
2025-05-20 11:23:49 +0900

    fix: mock message

--- tests/fixtures/discord.js/MockMessage.ts
+++ tests/fixtures/discord.js/MockMessage.ts
@@ -1,38 +1,32 @@
-import { GuildEmoji, type Message, MessageReaction, type PartialMessage, User } from "discord.js";
+import { type Message, type PartialMessage, User } from "discord.js";
 import { anything, instance, mock, verify, when } from "ts-mockito";
-
 export const mockMessage = (authorId: string, isPartial = false, isBotMessage = false) => {
 	const MessageMock = mock<Message<true>>();
 	const AuthorMock = mock(User);
-
 	if (isPartial) {
 		when(MessageMock.fetch()).thenReturn();
 	}
-
 	when(AuthorMock.bot).thenReturn(isBotMessage);
 	when(AuthorMock.id).thenReturn(authorId);
-
 	when(MessageMock.id).thenReturn("7890");
 	when(MessageMock.content).thenReturn("content");
 	when(MessageMock.author).thenReturn(instance(AuthorMock));
-
 	return MessageMock;
 };
-
 export const waitUntilMessageReply = async (message: Message | PartialMessage, timeout = 15000, atLeast = 1): Promise<void> => {
 	const startTime = Date.now();
 	return new Promise((resolve, reject) => {
-		const interval = setInterval(() => {
-			try {
+		const interval = setInterval(async () => {
+			await new Promise(() => {
 				verify(message.reply(anything())).atLeast(atLeast);
 				clearInterval(interval);
 				resolve();
-			} catch (error) {
+			}).catch((e) => {
 				if (Date.now() - startTime > timeout) {
 					clearInterval(interval);
 					reject(new Error("Timeout: Method was not called within the time limit."));
 				}
-			}
+			});
 		}, 100);
 	});
 };
