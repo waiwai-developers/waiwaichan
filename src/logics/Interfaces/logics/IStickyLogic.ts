5d0f74d401a4bd4a7a00d75cefdd00d476f096fc
mamepon2580 <g.poso.2580@gmail.com>
2025-05-21 11:13:31 +0900

    fix: ilogic

--- src/logics/Interfaces/logics/IStickyLogic.ts
+++ src/logics/Interfaces/logics/IStickyLogic.ts
@@ -2,6 +2,7 @@ import type { StickyDto } from "@/src/entities/dto/StickyDto";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
 import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
+import type { StickyMessage } from "@/src/entities/vo/StickyMessage";
 
 export interface IStickyLogic {
 	create(data: StickyDto): Promise<string>;
@@ -10,9 +11,14 @@ export interface IStickyLogic {
 		channelId: DiscordChannelId,
 	): Promise<StickyDto | undefined>;
 	delete(guildId: DiscordGuildId, channelId: DiscordChannelId): Promise<string>;
-	update(
+	updateMessageId(
 		guildId: DiscordGuildId,
 		channelId: DiscordChannelId,
 		messageId: DiscordMessageId,
+	): Promise<string>;
+	updateMessage(
+		guildId: DiscordGuildId,
+		channelId: DiscordChannelId,
+		message: StickyMessage,
 	): Promise<string>;
 }
