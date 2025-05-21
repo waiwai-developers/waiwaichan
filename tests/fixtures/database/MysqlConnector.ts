8b6a072c4ff8a2ed20f24db23a753b841a952980
mamepon2580 <g.poso.2580@gmail.com>
2025-05-21 18:11:06 +0900

    fix: linter

--- tests/fixtures/database/MysqlConnector.ts
+++ tests/fixtures/database/MysqlConnector.ts
@@ -1,40 +1,29 @@
 import { GetEnvDBConfig } from "@/src/entities/config/DatabaseConfig";
-import { Sequelize } from "sequelize-typescript";
-import { CandyRepositoryImpl } from "@/src/repositories/sequelize-mysql/CandyRepositoryImpl";
-import { CandyItemRepositoryImpl } from "@/src/repositories/sequelize-mysql/CandyItemRepositoryImpl";
-import { UserCandyItemRepositoryImpl } from "@/src/repositories/sequelize-mysql/UserCandyItemRepositoryImpl";
-import { ReminderRepositoryImpl } from "@/src/repositories/sequelize-mysql/ReminderRepositoryImpl";
-import { ThreadRepositoryImpl } from "@/src/repositories/sequelize-mysql/ThreadRepositoryImpl";
-import { SequelizeLogger } from "@/src/repositories/sequelize-mysql/SequelizeLogger";
-import type { Dialect } from "sequelize";
 import type { IDataBaseConnector } from "@/src/logics/Interfaces/repositories/database/IDataBaseConnector";
+import { CandyItemRepositoryImpl } from "@/src/repositories/sequelize-mysql/CandyItemRepositoryImpl";
+import { CandyRepositoryImpl } from "@/src/repositories/sequelize-mysql/CandyRepositoryImpl";
+import { ReminderRepositoryImpl } from "@/src/repositories/sequelize-mysql/ReminderRepositoryImpl";
+import { SequelizeLogger } from "@/src/repositories/sequelize-mysql/SequelizeLogger";
+import { ThreadRepositoryImpl } from "@/src/repositories/sequelize-mysql/ThreadRepositoryImpl";
+import { UserCandyItemRepositoryImpl } from "@/src/repositories/sequelize-mysql/UserCandyItemRepositoryImpl";
+import type { Dialect } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 
 export class MysqlConnector implements IDataBaseConnector<Sequelize, "mysql"> {
-    instance: Sequelize;
+	instance: Sequelize;
 
-    constructor() {
-        const dbConfig = GetEnvDBConfig();
-        this.instance = new Sequelize(
-            dbConfig.database,
-            dbConfig.username,
-            dbConfig.password,
-            {
-                host: dbConfig.host,
-                port: dbConfig.port,
-                dialect: dbConfig.dialect as Dialect,
-                logging: (s, t) => SequelizeLogger(s, t), // Pass undefined logger for tests
-                models: [
-                    CandyRepositoryImpl,
-                    CandyItemRepositoryImpl,
-                    UserCandyItemRepositoryImpl,
-                    ReminderRepositoryImpl,
-                    ThreadRepositoryImpl,
-                ],
-            },
-        );
-    }
+	constructor() {
+		const dbConfig = GetEnvDBConfig();
+		this.instance = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+			host: dbConfig.host,
+			port: dbConfig.port,
+			dialect: dbConfig.dialect as Dialect,
+			logging: (s, t) => SequelizeLogger(s, t), // Pass undefined logger for tests
+			models: [CandyRepositoryImpl, CandyItemRepositoryImpl, UserCandyItemRepositoryImpl, ReminderRepositoryImpl, ThreadRepositoryImpl],
+		});
+	}
 
-    getDBInstance(): Sequelize {
-        return this.instance;
-    }
+	getDBInstance(): Sequelize {
+		return this.instance;
+	}
 }
