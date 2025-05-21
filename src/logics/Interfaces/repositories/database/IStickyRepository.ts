8a1c7b8c7e32c13aed08e91149d1355dffa9b54d
mamepon2580 <g.poso.2580@gmail.com>
2025-05-21 11:13:57 +0900

    fix: irepository

--- src/logics/Interfaces/repositories/database/IStickyRepository.ts
+++ src/logics/Interfaces/repositories/database/IStickyRepository.ts
@@ -2,6 +2,7 @@ import type { StickyDto } from "@/src/entities/dto/StickyDto";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
 import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
 import type { DiscordMessageId } from "@/src/entities/vo/DiscordMessageId";
+import type { StickyMessage } from "@/src/entities/vo/StickyMessage";
 
 export interface IStickyRepository {
 	create(data: StickyDto): Promise<boolean>;
@@ -17,5 +18,10 @@ export interface IStickyRepository {
 		guildId: DiscordGuildId,
 		channelId: DiscordChannelId,
 		messageId: DiscordMessageId,
+	): Promise<boolean>;
+	updateForMessage(
+		guildId: DiscordGuildId,
+		channelId: DiscordChannelId,
+		message: StickyMessage,
 	): Promise<boolean>;
 }
