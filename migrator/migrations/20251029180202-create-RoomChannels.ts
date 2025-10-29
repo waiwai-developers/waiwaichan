aa466e1f4e2ae08104c4cf1abfc680ad05deaa8f
mamepon2580 <mamepon2580@gmail.com>
2025-10-29 19:13:36 +0900

    add: migrate room table

---
+++ migrator/migrations/20251029180202-create-RoomChannels.ts
@@ -0,0 +1,34 @@
+import type { Migration } from "@/migrator/umzug";
+import { DataTypes } from "sequelize";
+
+const TABLE_NAME = "RoomChannels";
+
+export const up: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().createTable(TABLE_NAME, {
+		guildId: {
+			allowNull: false,
+			primaryKey: true,
+			type: DataTypes.BIGINT,
+		},
+		channelid: {
+			allowNull: false,
+			primaryKey: true,
+			type: DataTypes.BIGINT,
+		},
+		createdAt: {
+			allowNull: false,
+			type: DataTypes.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: DataTypes.DATE,
+		},
+		deleteAt: {
+			allowNull: false,
+			type: DataTypes.DATE,
+		},
+	});
+};
+export const down: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().dropTable(TABLE_NAME);
+};
