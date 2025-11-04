c842b68d162d56abc4506663922e3a2fcd2831f2
mamepon2580 <mamepon2580@gmail.com>
2025-11-04 16:35:21 +0900

    delete: logic

--- src/logics/Interfaces/logics/IRoomChannelLogic.ts
+++
@@ -1,6 +0,0 @@
-import type { RoomChannelDto } from "@/src/entities/dto/RoomChannelDto";
-
-export interface IRoomChannelLogic {
-	create(data: RoomChannelDto): Promise<string>;
-	delete(data: RoomChannelDto): Promise<string>;
-}
