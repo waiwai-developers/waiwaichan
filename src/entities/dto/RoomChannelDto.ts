056d9ee0bcd8319a9f2bac963cbd8737b1d08fdf
mamepon2580 <mamepon2580@gmail.com>
2025-11-04 16:34:11 +0900

    delete: dto

--- src/entities/dto/RoomChannelDto.ts
+++
@@ -1,9 +0,0 @@
-import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
-import type { DiscordGuildId } from "@/src/entities/vo/DiscordGuildId";
-
-export class RoomChannelDto {
-	constructor(
-		public guildId: DiscordChannelId,
-		public channelId: DiscordGuildId,
-	) {}
-}
