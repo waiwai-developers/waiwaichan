84bf1e2009be83570b24f9b212675ec22c3a31c3
mamepon2580 <mamepon2580@gmail.com>
2025-11-04 16:35:37 +0900

    delete: repository

--- src/logics/Interfaces/repositories/database/IRoomChannelRepository.ts
+++
@@ -1,6 +0,0 @@
-import type { RoomChannelDto } from "@/src/entities/dto/RoomChannelDto";
-
-export interface IRoomChannelRepository {
-	create(data: RoomChannelDto): Promise<boolean>;
-	delete(data: RoomChannelDto): Promise<boolean>;
-}
