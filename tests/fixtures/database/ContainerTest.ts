7948204fd996f147b705e7dd27a56d7c93063d1b
mamepon2580 <mamepon2580@gmail.com>
2026-01-10 15:37:19 +0900

    fix: container test

--- tests/fixtures/database/ContainerTest.ts
+++ tests/fixtures/database/ContainerTest.ts
@@ -1,10 +1,7 @@
-import { GetTestDBConfig } from "@/src/entities/config/DatabaseConfig";
+import { DatabaseConfig } from "@/src/entities/config/DatabaseConfig";
 import { StartedMySqlContainer } from "@testcontainers/mysql";
 let container: StartedMySqlContainer;
 
 export const ContainerUp = async () => {
-	const testDBConfig = GetTestDBConfig();
-	if (!testDBConfig) {
-		throw new Error("Test database configuration not found: config/databasetest.json is required");
-	}
+	const testDBConfig = DatabaseConfig;
 };
