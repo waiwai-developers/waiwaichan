a5fb53154d8755425e39de3d20bb7430f6c0ff6a
mamepon2580 <mamepon2580@gmail.com>
2026-01-14 10:15:06 +0900

    fix: delete fook and container test

--- tests/fixtures/database/ContainerTest.ts
+++
@@ -1,7 +0,0 @@
-import { DatabaseConfig } from "@/src/entities/config/DatabaseConfig";
-import { StartedMySqlContainer } from "@testcontainers/mysql";
-let container: StartedMySqlContainer;
-
-export const ContainerUp = async () => {
-	const testDBConfig = DatabaseConfig;
-};
