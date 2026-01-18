00fa1c93729a9a17b8ed1be7c06801a93ea73f68
mamepon2580 <mamepon2580@gmail.com>
2026-01-18 22:22:11 +0900

    fix: scheducer.ts

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -2,17 +2,25 @@ import "reflect-metadata";
 import { AppConfig } from "@/src/entities/config/AppConfig";
 import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
 import { ReminderNotifyHandler } from "@/src/handlers/discord.js/events/ReminderNotifyHandler";
+import type { ILogger } from "@/src/logics/Interfaces/repositories/logger/ILogger";
 import type { MysqlSchedulerConnector } from "@/src/repositories/sequelize-mysql/MysqlSchedulerConnector";
 import { schedulerContainer } from "@/src/scheduler.di.config";
 import { Client, GatewayIntentBits } from "discord.js";
 import cron from "node-cron";
 
+const logger = schedulerContainer.get<ILogger>(RepoTypes.Logger);
 schedulerContainer.get<MysqlSchedulerConnector>(RepoTypes.DatabaseConnector);
 const client = new Client({
 	intents: Object.values(GatewayIntentBits).reduce(
 		(all, intent) => (Number.isNaN(intent) ? all : all | Number(intent)),
 		0,
 	),
+});
+
+client.on("ready", (c: Client) => {
+	if (c.isReady()) {
+		logger.info(`login: ${c.user.tag}`);
+	}
 });
 
 cron.schedule("* * * * *", () => ReminderNotifyHandler(client));
