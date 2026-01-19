0d05e03b6607aa03074864ddccf07444383df27e
mamepon2580 <mamepon2580@gmail.com>
2026-01-19 19:36:42 +0900

    fix: i data deletion repo

--- src/logics/Interfaces/repositories/database/IDataDeletionCircular.ts
+++ src/logics/Interfaces/repositories/database/IDataDeletionCircular.ts
@@ -1,9 +1,10 @@
+import type { ColumnName } from "@/src/entities/vo/ColumnName";
 import type { CommunityId } from "@/src/entities/vo/CommunityId";
 import type { UserId } from "@/src/entities/vo/UserId";
 
 export interface IDataDeletionCircular {
-	deleteRecordInRelatedTableCommunityId(
-		communityId: CommunityId,
+	deleteRecordInRelatedTable(
+		columnName: ColumnName,
+		id: CommunityId | UserId,
 	): Promise<boolean>;
-	deleteRecordInRelatedTableUserId(userId: UserId): Promise<boolean>;
 }
