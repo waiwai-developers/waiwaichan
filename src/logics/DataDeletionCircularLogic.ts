345b5e7f4a792043d56d43e4c405fa0bfb7ddfe3
mamepon2580 <mamepon2580@gmail.com>
2026-01-20 14:32:20 +0900

    fix: logic

--- src/logics/DataDeletionCircularLogic.ts
+++ src/logics/DataDeletionCircularLogic.ts
@@ -1,12 +1,12 @@
 import { RepoTypes } from "@/src/entities/constants/DIContainerTypes";
-import { ColumnName } from "@/src/entities/vo/ColumnName";
-import type { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
-import type { CommunityId } from "@/src/entities/vo/CommunityId";
-import type { UserClientId } from "@/src/entities/vo/UserClientId";
-import type { UserId } from "@/src/entities/vo/UserId";
 import type { IDataDeletionCircularLogic } from "@/src/logics/Interfaces/logics/IDataDeletionCircularLogic";
 import type { IDataDeletionCircular } from "@/src/logics/Interfaces/repositories/database/IDataDeletionCircular";
 import { inject, injectable } from "inversify";
+import type { UserId } from "@/src/entities/vo/UserId";
+import type { CommunityId } from "@/src/entities/vo/CommunityId";
+import  { ColumnDto } from "@/src/entities/dto/Column";
+import  { ColumnName } from "@/src/entities/vo/ColumnName";
+import  { ColumnId } from "@/src/entities/vo/ColumnId";
 
 @injectable()
 export class DataDeletionCircularLogic implements IDataDeletionCircularLogic {
@@ -14,23 +14,18 @@ export class DataDeletionCircularLogic implements IDataDeletionCircularLogic {
 	private readonly dataDeletionCircular!: IDataDeletionCircular;
 
 	async deleteRecordInRelatedTableCommunityId(
-		id: CommunityId,
-		clientId: CommunityClientId,
+		userId: UserId,
 	): Promise<boolean> {
 		return await this.dataDeletionCircular.deleteRecordInRelatedTable(
-			[new ColumnName("communityId"), id],
-			[new ColumnName("guildId"), clientId],
+			new ColumnDto(ColumnName.user ,new ColumnId(userId.getValue()))
 		);
 	}
 
 	async deleteRecordInRelatedTableUserId(
-		id: UserId,
-		clientId: UserClientId,
+		communityId: CommunityId,
 	): Promise<boolean> {
 		return await this.dataDeletionCircular.deleteRecordInRelatedTable(
-			[new ColumnName("userId"), id],
-			[new ColumnName("userId"), clientId],
-			[new ColumnName("receiveUserId"), clientId],
+			new ColumnDto(ColumnName.community ,new ColumnId(communityId.getValue()))
 		);
 	}
 }
--- src/logics/Interfaces/logics/IDataDeletionCircularLogic.ts
+++ src/logics/Interfaces/logics/IDataDeletionCircularLogic.ts
@@ -1,15 +1,11 @@
-import type { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
 import type { CommunityId } from "@/src/entities/vo/CommunityId";
-import type { UserClientId } from "@/src/entities/vo/UserClientId";
 import type { UserId } from "@/src/entities/vo/UserId";
 
 export interface IDataDeletionCircularLogic {
 	deleteRecordInRelatedTableCommunityId(
-		id: CommunityId,
-		clientId: CommunityClientId,
+		communityId: CommunityId,
 	): Promise<boolean>;
 	deleteRecordInRelatedTableUserId(
-		id: UserId,
-		clientId: UserClientId,
+		userId: UserId,
 	): Promise<boolean>;
 }
