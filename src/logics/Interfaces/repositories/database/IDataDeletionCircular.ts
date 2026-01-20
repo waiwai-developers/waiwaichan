f9fd0cb49212efdf562525b98a75bb47187fd827
mamepon2580 <mamepon2580@gmail.com>
2026-01-20 14:32:52 +0900

    fix: repository

--- src/logics/Interfaces/repositories/database/IDataDeletionCircular.ts
+++ src/logics/Interfaces/repositories/database/IDataDeletionCircular.ts
@@ -1,14 +1,7 @@
-import type { ColumnName } from "@/src/entities/vo/ColumnName";
-import type { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
-import type { CommunityId } from "@/src/entities/vo/CommunityId";
-import type { UserClientId } from "@/src/entities/vo/UserClientId";
-import type { UserId } from "@/src/entities/vo/UserId";
+import type { ColumnDto } from "@/src/entities/dto/Column";
 
 export interface IDataDeletionCircular {
 	deleteRecordInRelatedTable(
-		...columnAndIds: Array<[
-			ColumnName,
-			CommunityId | CommunityClientId | UserId | UserClientId,
-		]>
+		data: ColumnDto
 	): Promise<boolean>;
 }
--- src/repositories/sequelize-mysql/DataDeletionCircularImpl.ts
+++ src/repositories/sequelize-mysql/DataDeletionCircularImpl.ts
@@ -1,60 +1,35 @@
-import type { ColumnName } from "@/src/entities/vo/ColumnName";
-import type { CommunityClientId } from "@/src/entities/vo/CommunityClientId";
-import type { CommunityId } from "@/src/entities/vo/CommunityId";
-import type { UserClientId } from "@/src/entities/vo/UserClientId";
-import type { UserId } from "@/src/entities/vo/UserId";
 import type { IDataDeletionCircular } from "@/src/logics/Interfaces/repositories/database/IDataDeletionCircular";
 import { injectable } from "inversify";
 import type { Model, ModelStatic } from "sequelize";
 import { MysqlConnector } from "./MysqlConnector";
 import { MysqlSchedulerConnector } from "./MysqlSchedulerConnector";
+import type { ColumnDto } from "@/src/entities/dto/Column";
 
 @injectable()
 class DataDeletionCircularImpl implements IDataDeletionCircular {
 	async deleteRecordInRelatedTable(
-		...columnAndIds: Array<[
-			ColumnName,
-			CommunityId | CommunityClientId | UserId | UserClientId,
-		]>
+		data: ColumnDto
 	): Promise<boolean> {
 		try {
-			const columnValues = columnAndIds.map(([columnName]) =>
-				columnName.getValue(),
-			);
-			const idValues = columnAndIds.map(([, id]) => id.getValue());
-			const relatedModels = this.getRelatedModels(columnValues);
+			const columnName = data.columnName.getValue();
+			const columnId = data.columnId.getValue();
+			const relatedModels = this.getRelatedModels([columnName]);
 
 			if (relatedModels.length === 0) {
 				return true;
 			}
 
-			return await Promise.all(
-				relatedModels.map((model) => {
-					const attributes = Object.keys(model.getAttributes());
-					const deletions = columnValues
-						.map((columnValue, index) => ({
-							columnValue,
-							idValue: idValues[index],
-						}))
-						.filter(({ columnValue }) => attributes.includes(columnValue))
-						.map(({ columnValue, idValue }) =>
-							model.destroy({
-								where: {
-									[columnValue]: idValue,
-								},
-							}),
-						);
-
-					return deletions.length > 0
-						? Promise.all(deletions)
-						: Promise.resolve([0]);
-				}),
-			).then((results) =>
-				results
-					.flat()
-					.map((res) => res >= 0)
-					.reduce((acc, curr) => acc && curr, true),
+			const results = await Promise.all(
+				relatedModels.map((model) =>
+					model.destroy({
+						where: {
+							[columnName]: columnId,
+						},
+					}),
+				),
 			);
+
+			return results.every((res) => res >= 0);
 		} catch (error) {
 			console.error(`Error in deleteRecordInRelatedTable: ${error}`);
 			return false;
