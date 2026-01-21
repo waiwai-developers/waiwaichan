73d9f26ae81d110988bd206f52334f4dd21e12f2
mamepon2580 <mamepon2580@gmail.com>
2026-01-21 11:24:18 +0900

    fix: dto and vo

--- src/entities/dto/ReminderDto.ts
+++ src/entities/dto/ReminderDto.ts
@@ -1,10 +1,6 @@
 import type { CommunityId } from "@/src/entities/vo/CommunityId";
 import type { DiscordChannelId } from "@/src/entities/vo/DiscordChannelId";
-<<<<<<< Updated upstream
 import type { UserId } from "@/src/entities/vo/UserId";
-=======
-import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
->>>>>>> Stashed changes
 import type { ReceiveDiscordUserName } from "@/src/entities/vo/ReceiveDiscordUserName";
 import type { RemindTime } from "@/src/entities/vo/RemindTime";
 import type { ReminderId } from "@/src/entities/vo/ReminderId";
