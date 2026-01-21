88adc8e1d323b3e395e519947ab788a87282f427
mamepon2580 <mamepon2580@gmail.com>
2026-01-21 11:25:27 +0900

    fix: reopo

--- src/logics/Interfaces/repositories/database/IReminderRepository.ts
+++ src/logics/Interfaces/repositories/database/IReminderRepository.ts
@@ -1,10 +1,6 @@
 import type { ReminderDto } from "@/src/entities/dto/ReminderDto";
 import type { CommunityId } from "@/src/entities/vo/CommunityId";
-<<<<<<< Updated upstream
 import type { UserId } from "@/src/entities/vo/UserId";
-=======
-import type { DiscordUserId } from "@/src/entities/vo/DiscordUserId";
->>>>>>> Stashed changes
 import type { ReminderId } from "@/src/entities/vo/ReminderId";
 
 export interface IReminderRepository {
@@ -12,18 +8,10 @@ export interface IReminderRepository {
 	deleteReminder(
 		id: ReminderId,
 		communityId: CommunityId,
-<<<<<<< Updated upstream
 		userId: UserId,
 	): Promise<boolean>;
 	findByUserId(
 		communityId: CommunityId,
 		userId: UserId,
-=======
-		userId: DiscordUserId,
-	): Promise<boolean>;
-	findByUserId(
-		communityId: CommunityId,
-		userId: DiscordUserId,
->>>>>>> Stashed changes
 	): Promise<ReminderDto[]>;
 }
