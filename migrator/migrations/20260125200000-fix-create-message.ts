edbbf4f79cc565dedc1888666ab73cf66c8782dd
mamepon2580 <mamepon2580@gmail.com>
2026-01-24 20:45:28 +0900

    fix: migrate

---
+++ migrator/migrations/20260125200000-fix-create-message.ts
@@ -0,0 +1,53 @@
+import type { Migration } from "@/migrator/umzug";
+import { DataTypes } from "sequelize";
+
+const TABLE_NAME = "Messages";
+
+export const up: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().createTable(TABLE_NAME, {
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: DataTypes.INTEGER,
+		},
+		categoryType: {
+			allowNull: false,
+			type: DataTypes.INTEGER,
+		},
+		clientId: {
+			allowNull: false,
+			type: DataTypes.BIGINT,
+		},
+		communityId: {
+			allowNull: false,
+			type: DataTypes.INTEGER,
+		},
+		channelId: {
+			allowNull: false,
+			type: DataTypes.INTEGER,
+		},
+		userId: {
+			allowNull: false,
+			type: DataTypes.INTEGER,
+		},
+		batchStatus: {
+			allowNull: false,
+			type: DataTypes.INTEGER,
+		},
+		deletedAt: {
+			type: DataTypes.DATE,
+		},
+		createdAt: {
+			allowNull: false,
+			type: DataTypes.DATE,
+		},
+		updatedAt: {
+			allowNull: false,
+			type: DataTypes.DATE,
+		},
+	});
+};
+export const down: Migration = async ({ context: sequelize }) => {
+	await sequelize.getQueryInterface().dropTable(TABLE_NAME);
+};
