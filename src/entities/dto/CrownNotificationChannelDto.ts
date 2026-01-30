d5cbd467baf99fcecb2dc5b6a0a08a2fc182ea8a
mamepon2580 <mamepon2580@gmail.com>
2026-01-30 16:46:40 +0900

    add: crown dto

---
+++ src/entities/dto/CrownNotificationChannelDto.ts
@@ -0,0 +1,9 @@
+import type { ChannelId } from "@/src/entities/vo/ChannelId";
+import type { CommunityId } from "@/src/entities/vo/CommunityId";
+
+export class CrownNotificationChannelDto {
+	constructor(
+		public communityId: CommunityId,
+		public channelId: ChannelId,
+	) {}
+}
