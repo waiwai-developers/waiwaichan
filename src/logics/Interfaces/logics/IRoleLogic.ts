2165d0adf987eabcf308ffe6bacadeb5d30feaa9
mamepon2580 <mamepon2580@gmail.com>
2026-02-01 20:53:09 +0900

    add: logic

---
+++ src/logics/Interfaces/logics/IRoleLogic.ts
@@ -0,0 +1,33 @@
+import type { RoleDto } from "@/src/entities/dto/RoleDto";
+import type { DeletedRoleTargetDto } from "@/src/entities/dto/DeletedRoleTargetDto";
+import type { RoleClientId } from "@/src/entities/vo/RoleClientId";
+import type { RoleCommunityId } from "@/src/entities/vo/RoleCommunityId";
+import type { RoleId } from "@/src/entities/vo/RoleId";
+
+export interface IRoleLogic {
+	bulkCreate(data: RoleDto[]): Promise<boolean>;
+	deletebyCommunityId(communityId: RoleCommunityId): Promise<boolean>;
+	deleteByCommunityIdAndClientId(
+		communityId: RoleCommunityId,
+		clientId: RoleClientId,
+	): Promise<boolean>;
+	deletebyClientId(
+		communityId: RoleCommunityId,
+		clientId: RoleClientId,
+	): Promise<boolean>;
+	getId(data: RoleDto): Promise<RoleId | undefined>;
+	getIdByCommunityIdAndClientId(
+		communityId: RoleCommunityId,
+		clientId: RoleClientId,
+	): Promise<RoleId | undefined>;
+	getClientIdById(id: RoleId): Promise<RoleClientId | undefined>;
+	deleteNotBelongByCommunityIdAndClientIds(
+		communityId: RoleCommunityId,
+		clientIds: RoleClientId[],
+	): Promise<boolean>;
+	findByBatchStatusAndDeletedAt(): Promise<RoleId[]>;
+	findDeletionTargetsByBatchStatusAndDeletedAt(): Promise<
+		DeletedRoleTargetDto[]
+	>;
+	updatebatchStatus(id: RoleId): Promise<boolean>;
+}
