7e5d6aab75993f18103c87425f161e23d1b3040b
mamepon2580 <mamepon2580@gmail.com>
2026-02-04 19:03:34 +0900

    add: constants

---
+++ src/entities/constants/PredefinedRoles.ts
@@ -0,0 +1,17 @@
+export const PersonalityContextsConst: {
+	PredefinedRole: {
+		id: number;
+		name: string;
+	}[];
+} = {
+	PredefinedRole: [
+		{
+			id: 1,
+			name: "admin",
+		},
+		{
+			id: 2,
+			name: "user",
+		},
+	],
+};
